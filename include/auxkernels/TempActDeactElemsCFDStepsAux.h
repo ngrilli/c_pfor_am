669e3260ac7dfc3294776372cc6be2e2b28c9175
ngrilli <ngrilli@purdue.edu>
2021-01-03 18:32:15 +0800

    Update phase only at CFD time steps

---
+++ include/auxkernels/TempActDeactElemsCFDStepsAux.h
@@ -0,0 +1,42 @@
+// Nicolò Grilli
+// 3 Gennaio 2021
+// National University of Singapore
+
+#pragma once
+
+#include "AuxKernel.h"
+#include "LaserTempReadFile.h"
+
+// Read temperature due to laser scan during SLM from CFD simulation
+// Temperature is changed only every CFD time step for phase transformation
+// Therefore, no linear interpolation in time
+
+// Forward declarations
+
+class TempActDeactElemsCFDStepsAux : public AuxKernel
+{
+public:
+  static InputParameters validParams();
+
+  TempActDeactElemsCFDStepsAux(const InputParameters & parameters);
+  virtual ~TempActDeactElemsCFDStepsAux() {}
+
+protected:
+
+  /// Base name of the material system used to calculate the temperature
+  const std::string _base_name;
+  
+  /// The LaserTempReadFile GeneralUserObject to read element specific temperature values from file
+  const LaserTempReadFile * const _temperature_read_user_object;
+  
+  /// Time interval between two temperature data field
+  const Real _temperature_time_step;
+
+  const Real _melting_temperature_high;
+  const Real _melting_temperature_low;
+  const Real _gas_temperature_high;
+  const Real _gas_temperature_low;
+
+  virtual Real computeValue();
+
+};
---
+++ src/auxkernels/TempActDeactElemsCFDStepsAux.C
@@ -0,0 +1,70 @@
+// Nicolò Grilli
+// 3 Gennaio 2021
+// National University of Singapore
+
+#include "TempActDeactElemsCFDStepsAux.h"
+
+#include <cmath>
+
+registerMooseObject("TensorMechanicsApp", TempActDeactElemsCFDStepsAux);
+
+InputParameters
+TempActDeactElemsCFDStepsAux::validParams()
+{
+  InputParameters params = AuxKernel::validParams();
+  params.addClassDescription("Read temperature due to laser scan during SLM from CFD simulation");
+  params.addParam<std::string>("base_name", "Mechanical property base name");
+  params.addParam<UserObjectName>("temperature_read_user_object",
+                                  "The LaserTempReadFile "
+                                  "GeneralUserObject to read element "
+                                  "specific temperature values from file");
+  params.addRequiredParam<Real>("temperature_time_step","Time interval between two temperature data field");
+  params.addParam<Real>("melting_temperature_high", 1673.15, "Melting temperature (liquidus) = zero stiffness.");  
+  params.addParam<Real>("melting_temperature_low", 1648.15, "Solidus = full stiffness.");
+  params.addParam<Real>("gas_temperature_high", 298.1, "Lowest possible solid temperature = full stiffness.");
+  params.addParam<Real>("gas_temperature_low", 298.0, "Gas temperature = zero stiffness.");
+  return params;
+}
+
+TempActDeactElemsCFDStepsAux::TempActDeactElemsCFDStepsAux(const InputParameters & parameters)
+  : AuxKernel(parameters),
+    _base_name(isParamValid("base_name") ? getParam<std::string>("base_name") + "_" : ""),
+	_temperature_read_user_object(isParamValid("temperature_read_user_object")
+                                  ? &getUserObject<LaserTempReadFile>("temperature_read_user_object")
+                                  : nullptr),
+    _temperature_time_step(getParam<Real>("temperature_time_step")),
+	_melting_temperature_high(getParam<Real>("melting_temperature_high")),
+	_melting_temperature_low(getParam<Real>("melting_temperature_low")),
+	_gas_temperature_high(getParam<Real>("gas_temperature_high")),
+	_gas_temperature_low(getParam<Real>("gas_temperature_low"))
+{
+}
+
+// Calculate temperature read from file based on element index
+// It can be used with GeneratedMesh
+Real
+TempActDeactElemsCFDStepsAux::computeValue()
+{
+  Real TempValue = 0.0;
+  unsigned int temperature_step;
+  
+  // determine time step to be used from the CFD simulations
+  temperature_step = floor(_t / _temperature_time_step);
+	
+  if (_temperature_read_user_object)
+  {
+	// Temperature to determine the phase transformation
+	// is the one at the previous CFD time step
+    TempValue = _temperature_read_user_object->getData(_current_elem, temperature_step);
+
+	// Limit temperature in the interval gas to liquidus
+    TempValue = std::min(_melting_temperature_high,TempValue);
+	TempValue = std::max(_gas_temperature_low,TempValue);
+  }
+  else
+  {
+	mooseError("Error in reading temperature file");
+  }
+  
+  return TempValue;
+}
