557e0216d81ee8c24ff920bec70c99c9e24c1f55
ngrilli <ngrilli@purdue.edu>
2021-07-06 15:30:11 +0100

    AdvectionCoupled with constant V to use together with DG kernel

---
+++ include/kernels/AdvectionCoupledVConst.h
@@ -0,0 +1,46 @@
+// Nicolo Grilli
+// University of Bristol
+// 6 Luglio 2021
+
+#pragma once
+
+#include "Kernel.h"
+
+// Forward Declaration
+class AdvectionCoupledVConst;
+
+/**
+ * Coupled advection of the variable by the velocity provided by the user.
+ * Options for numerical stabilization are: none; full upwinding
+ */
+template <>
+InputParameters validParams<AdvectionCoupledVConst>();
+
+class AdvectionCoupledVConst : public Kernel
+{
+public:
+  static InputParameters validParams();
+
+  AdvectionCoupledVConst(const InputParameters & parameters);
+
+protected:
+  /// Returns - _grad_test * velocity
+  Real negSpeedQp() const;
+  
+  virtual Real computeQpResidual() override;
+  virtual Real computeQpJacobian() override;
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar) override;
+  virtual void computeResidual() override;
+  virtual void computeJacobian() override;
+  virtual void computeOffDiagJacobian(unsigned int jvar) override;
+
+  /// advection velocity
+  const VectorVariableValue & _velocity;
+  
+  // Coupled dislocation density in the flux term
+  const VariableValue & _rho_coupled;
+  
+  const bool _rho_coupled_coupled;
+  unsigned int _rho_coupled_var; 
+
+};
---
+++ src/kernels/AdvectionCoupledVConst.C
@@ -0,0 +1,88 @@
+// Nicolo Grilli
+// University of Bristol
+// 6 Luglio 2021
+
+#include "AdvectionCoupledVConst.h"
+#include "SystemBase.h"
+
+registerMooseObject("MooseApp", AdvectionCoupledVConst);
+
+defineLegacyParams(AdvectionCoupledVConst);
+
+InputParameters
+AdvectionCoupledVConst::validParams()
+{
+  InputParameters params = Kernel::validParams();
+  params.addClassDescription("Coupled Advection. "
+							 "Constant velocity. ");
+  params.addRequiredCoupledVar("velocity", "Velocity vector");
+  params.addCoupledVar("rho_coupled", 0.0, "Coupled dislocation density in the flux term.");
+  return params;
+}
+
+AdvectionCoupledVConst::AdvectionCoupledVConst(const InputParameters & parameters)
+  : Kernel(parameters),
+    _velocity(coupledVectorValue("velocity")),
+    _rho_coupled(coupledValue("rho_coupled")), // Coupled dislocation density in the flux term
+    _rho_coupled_coupled(isCoupled("rho_coupled")),
+    _rho_coupled_var(_rho_coupled_coupled ? coupled("rho_coupled") : 0)
+{
+}
+
+Real
+AdvectionCoupledVConst::negSpeedQp() const
+{
+  return -_grad_test[_i][_qp] * _velocity[_qp];
+}
+
+Real
+AdvectionCoupledVConst::computeQpResidual()
+{
+  // This is the no-upwinded version
+  // It gets called via Kernel::computeResidual()
+  return negSpeedQp() * _rho_coupled[_qp];
+}
+
+Real
+AdvectionCoupledVConst::computeQpJacobian()
+{
+  // This is the no-upwinded version
+  // It gets called via Kernel::computeJacobian()
+  return 0.0;
+}
+
+Real
+AdvectionCoupledVConst::computeQpOffDiagJacobian(unsigned int jvar)
+{
+  // It gets called via Kernel::computeOffDiagJacobian()
+  
+  if (_rho_coupled_coupled && jvar == _rho_coupled_var)
+  {
+		
+    return negSpeedQp() * _phi[_j][_qp];
+	
+  }
+  else {
+	  
+	return 0.0;
+	
+  }
+}
+
+void
+AdvectionCoupledVConst::computeResidual()
+{
+  Kernel::computeResidual();
+}
+
+void
+AdvectionCoupledVConst::computeJacobian()
+{
+  Kernel::computeJacobian();
+}
+
+void
+AdvectionCoupledVConst::computeOffDiagJacobian(const unsigned int jvar_num)
+{
+  Kernel::computeOffDiagJacobian(jvar_num);
+}
