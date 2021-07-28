0aec92632003f7ed14282994777f0d00a89e61d5
ngrilli <ngrilli@purdue.edu>
2021-07-28 19:33:45 +0100

    Reordering in variables in FiniteStrainCrystalPlasticityUranium.h

--- include/materials/FiniteStrainCrystalPlasticityUranium.h
+++ include/materials/FiniteStrainCrystalPlasticityUranium.h
@@ -82,16 +82,17 @@ protected:
   
   const Real _k_bol; // Boltzmann constant, k in equation (8) of the CMS paper
   const Real _da0; // Constant dislocation annihilation length, \hat{d}_\alpha^0 in equation (8)
-  const Real _ka; // Pre-factor for dislocation multiplication, k_\alpha^1 in equation (6)
-  const Real _burgers_vector; // Burgers vector magnitude, b_\alpha in equation (5)
-  const Real _projected_mu; // Projected shear modulus on the slip systems, \mu_\alpha in eq. (5)
-  const Real _tau0; // Constant friction stress, tau_\alpha^0 in equation (5) of the CMS paper
   
   // Logarithm of the strain rate ratio in equation (8) of the CMS paper
   // \ln \left ( \frac{ \dot{\varepsilon}_0 }{ \dot{\varepsilon} } \right )
   const Real _log_strain_rate_ratio;
   
-  const Real _drag_stress; // Drag stress, D_\alpha in equation (8) of the CMS paper
+  const Real _drag_stress; // Drag stress, D_\alpha in equation (8) of the CMS paper  
+  
+  const Real _ka; // Pre-factor for dislocation multiplication, k_\alpha^1 in equation (6)
+  const Real _burgers_vector; // Burgers vector magnitude, b_\alpha in equation (5)
+  const Real _projected_mu; // Projected shear modulus on the slip systems, \mu_\alpha in eq. (5)
+  const Real _tau0; // Constant friction stress, tau_\alpha^0 in equation (5) of the CMS paper
   
   // critical resolved shear stress
   // exponentially decreased with temperature
