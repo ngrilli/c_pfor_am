f82e495e8b872c3b42edb625ddeffd6769f33d81
ngrilli <ngrilli@purdue.edu>
2021-08-06 17:53:27 +0100

    Residual and Jacobian for DG curvature advection

---
+++ include/dgkernels/DGCurvatureAdvectionPN.h
@@ -0,0 +1,96 @@
+// Nicolo Grilli
+// University of Bristol
+// 4 Agosto 2021
+
+// DG Dislocation curvature advection.
+// Upwind condition is calculated both on edge/screw dislocations
+// in this element and on the neighbouring element.
+// The forward and backward motion of
+// positive and negative GND is taken into account.
+// This kernel represents the first term on the right-hand side 
+// of Eq. (12c) in:
+// Katrin Schulz, Lydia Wagner, Christian Wieners
+// A mesoscale continuum approach of dislocation dynamics and the
+// approximation by a Runge-Kutta discontinuous Galerkin method
+// International Journal of Plasticity 120 (2019) 248–261
+
+#pragma once
+
+#include "DGKernel.h"
+
+class DGCurvatureAdvectionPN;
+
+template <>
+InputParameters validParams<DGCurvatureAdvectionPN>();
+
+class DGCurvatureAdvectionPN : public DGKernel
+{
+public:
+  static InputParameters validParams();
+
+  DGCurvatureAdvectionPN(const InputParameters & parameters);
+
+protected:
+  virtual void getDislocationVelocity();
+  virtual Real computeQpResidual(Moose::DGResidualType type) override;
+  virtual Real computeQpJacobian(Moose::DGJacobianType type) override;
+  virtual Real computeQpOffDiagJacobian(Moose::DGJacobianType type, unsigned int jvar) override;
+
+  /// advection velocity
+  RealVectorValue _velocity;
+  
+  // GND dislocation density: rho_x or rho_y for edge or screw
+  const VariableValue & _rho_gnd;
+  
+  const bool _rho_gnd_coupled;
+  unsigned int _rho_gnd_var; 
+
+  // Total dislocation density: rho_t 
+  const VariableValue & _rho_tot;
+  
+  const bool _rho_tot_coupled;
+  unsigned int _rho_tot_var;
+  
+  // Coupled dislocation densities in the neighbouring element
+  const VariableValue & _rho_gnd_neighbor;
+  const VariableValue & _rho_tot_neighbor;
+  
+  // Coupled dislocation density of the other type
+  // If _rho_gnd is edge dislocations
+  // this coupled variable is screw dislocation density and vice versa
+  // This is used to calculate the fraction of GND density _rho_gnd
+  // compared with the total edge + screw GND density
+  // the following relationship applies for the variables in Hochrainer model
+  // in the case of a purely signed dislocation (mixed edge/screw, no SSD):
+  // rho_t = sqrt(rho_e^2 + rho_s^2)
+  const VariableValue & _rho_gnd_ot;
+  
+  const bool _rho_gnd_ot_coupled;
+  unsigned int _rho_gnd_ot_var;
+  
+  // Coupled dislocation density of the other type in the neighbouring element
+  const VariableValue & _rho_gnd_ot_neighbor;
+
+  // Tolerance on small values of rho_tot
+  const Real _rho_tot_tol;
+  
+  // Edge slip directions of all slip systems
+  const MaterialProperty<std::vector<Real>> & _edge_slip_direction;
+
+  // Screw slip directions of all slip systems
+  const MaterialProperty<std::vector<Real>> & _screw_slip_direction;
+  
+  // Dislocation velocity value (signed) on all slip systems
+  const MaterialProperty<std::vector<Real>> & _dislo_velocity;
+
+  // Slip system index to determine slip direction
+  const unsigned int _slip_sys_index;
+  
+  // Character of dislocations (edge or screw)
+  const enum class DisloCharacter { edge, screw } _dislo_character;
+  
+  // No need to check that variable _u on which the kernel is applied is
+  // GND density or total density like in DGAdvectionCoupled
+  // because this kernel is applied only on the curvature
+  
+};
---
+++ src/dgkernels/DGCurvatureAdvectionPN.C
@@ -0,0 +1,478 @@
+// Nicolo Grilli
+// University of Bristol
+// 4 Agosto 2021
+
+#include "DGCurvatureAdvectionPN.h"
+
+registerMooseObject("MooseApp", DGCurvatureAdvectionPN);
+
+defineLegacyParams(DGCurvatureAdvectionPN);
+
+InputParameters
+DGCurvatureAdvectionPN::validParams()
+{
+  InputParameters params = DGKernel::validParams();
+  params.addClassDescription("DG Dislocation curvature advection."
+							 "Upwind condition is calculated both on edge/screw dislocations "
+							 "in this element and on the neighbouring element. "
+							 "The forward and backward motion of "
+							 "positive and negative GND is taken into account. ");
+  params.addCoupledVar("rho_gnd", 0.0, "GND dislocation density: rho_x or rho_y for edge or screw.");
+  params.addCoupledVar("rho_gnd_ot", 0.0, "Other type: screw for edge kernel and vice versa.");
+  params.addCoupledVar("rho_tot", 0.0, "Total dislocation density: rho_t.");
+  params.addParam<Real>("rho_tot_tol",0.000001,"Tolerance on small values of rho_tot.");
+  params.addRequiredParam<int>("slip_sys_index", "Slip system index to determine slip direction "
+							   "for instance from 0 to 11 for FCC.");
+  MooseEnum dislo_character("edge screw", "edge");
+  params.addRequiredParam<MooseEnum>("dislo_character",
+                                     dislo_character,
+                                     "Character of dislocations: edge or screw.");
+  return params;
+}
+
+DGCurvatureAdvectionPN::DGCurvatureAdvectionPN(const InputParameters & parameters)
+  : DGKernel(parameters),
+    _rho_gnd(coupledValue("rho_gnd")), // GND dislocation density: rho_x or rho_y for edge or screw
+    _rho_gnd_coupled(isCoupled("rho_gnd")),
+    _rho_gnd_var(_rho_gnd_coupled ? coupled("rho_gnd") : 0),
+    _rho_tot(coupledValue("rho_tot")), // Total dislocation density: rho_t
+    _rho_tot_coupled(isCoupled("rho_tot")),
+    _rho_tot_var(_rho_tot_coupled ? coupled("rho_tot") : 0),
+	_rho_gnd_neighbor(coupledNeighborValue("rho_gnd")),
+    _rho_tot_neighbor(coupledNeighborValue("rho_tot")),
+    _rho_gnd_ot(coupledValue("rho_gnd_ot")), // Other type: screw for edge kernel and vice versa 
+    _rho_gnd_ot_coupled(isCoupled("rho_gnd_ot")),
+    _rho_gnd_ot_var(_rho_gnd_ot_coupled ? coupled("rho_gnd_ot") : 0),
+	_rho_gnd_ot_neighbor(coupledNeighborValue("rho_gnd_ot")),
+	_rho_tot_tol(getParam<Real>("rho_tot_tol")), // tolerance on small values of the total dislocation density
+    _edge_slip_direction(getMaterialProperty<std::vector<Real>>("edge_slip_direction")), // Edge velocity direction
+	_screw_slip_direction(getMaterialProperty<std::vector<Real>>("screw_slip_direction")), // Screw velocity direction
+    _dislo_velocity(getMaterialProperty<std::vector<Real>>("dislo_velocity")), // Velocity value (signed)
+	_slip_sys_index(getParam<int>("slip_sys_index")),
+	_dislo_character(getParam<MooseEnum>("dislo_character").getEnum<DisloCharacter>())
+{
+}
+
+// read dislocation velocity from material object
+// and store in _velocity
+void
+DGCurvatureAdvectionPN::getDislocationVelocity()
+{
+	
+  // Find dislocation velocity based on slip systems index and dislocation character
+  switch (_dislo_character)
+  {
+    case DisloCharacter::edge:
+	  for (unsigned int j = 0; j < LIBMESH_DIM; ++j)
+	  {
+	    _velocity(j) = _edge_slip_direction[_qp][_slip_sys_index * LIBMESH_DIM + j]; // edge direction	  
+	  }
+	  break;
+	case DisloCharacter::screw:
+	  for (unsigned int j = 0; j < LIBMESH_DIM; ++j)
+	  {
+		// note that the definition of _screw_slip_direction in FiniteStrainCrystalPlasticityDislo
+		// is -y, because +x is _edge_slip_direction and +z is slip plane normal
+		// but derivative must be taken along +y
+		// therefore a sign change is needed
+	    _velocity(j) = - _screw_slip_direction[_qp][_slip_sys_index * LIBMESH_DIM + j]; // screw direction	  
+	  }	
+	  break;
+  }
+  
+  for (unsigned int j = 0; j < LIBMESH_DIM; ++j)
+  {
+	_velocity(j) *= _dislo_velocity[_qp][_slip_sys_index]; // velocity value (signed)
+  }
+	
+}
+
+Real
+DGCurvatureAdvectionPN::computeQpResidual(Moose::DGResidualType type)
+{ 
+  Real r = 0; // Residual for output
+  Real rho_gnd_pos; // positive GND density in this element
+  Real rho_gnd_neg; // negative GND density in this element
+  Real neigh_rho_gnd_pos; // positive GND density in the neighbouring element
+  Real neigh_rho_gnd_neg; // negative GND density in the neighbouring element
+  
+  getDislocationVelocity();
+
+  Real vdotn = _velocity * _normals[_qp];
+  
+  // Define positive and negative GND densities
+  // both are positive quantities
+  // _u[_qp] is q_tot in this case
+  // _rho_tot[_qp] is rho_tot in this case
+  // _rho_gnd[_qp] is edge or screw, respectively
+  // _rho_gnd_ot[_qp] is screw or edge, respectively
+  // rho_t = sqrt(rho_e^2 + rho_s^2) in the pure GND case
+  
+  rho_gnd_pos = 0.5 * (std::sqrt(_rho_tot[_qp]*_rho_tot[_qp] 
+	          - _rho_gnd_ot[_qp]*_rho_gnd_ot[_qp]) + _rho_gnd[_qp]);
+				  
+  rho_gnd_neg = 0.5 * (std::sqrt(_rho_tot[_qp]*_rho_tot[_qp] 
+	          - _rho_gnd_ot[_qp]*_rho_gnd_ot[_qp]) - _rho_gnd[_qp]);
+				  
+  neigh_rho_gnd_pos = 0.5 * (std::sqrt(_rho_tot_neighbor[_qp]*_rho_tot_neighbor[_qp]
+	                - _rho_gnd_ot_neighbor[_qp]*_rho_gnd_ot_neighbor[_qp]) 
+				    + _rho_gnd_neighbor[_qp]);
+						
+  neigh_rho_gnd_neg = 0.5 * (std::sqrt(_rho_tot_neighbor[_qp]*_rho_tot_neighbor[_qp]
+	                - _rho_gnd_ot_neighbor[_qp]*_rho_gnd_ot_neighbor[_qp]) 
+	                - _rho_gnd_neighbor[_qp]);
+
+  // residual calculation: this is the curvature residual
+  // if a positive curvature exits the element, residual must be positive 
+  // if a positive curvature enters the element, residual must be negative
+  // There is no dependence on the sign of the gnd considered  
+
+  switch (type)
+  {
+    case Moose::Element:
+	  	  
+	  if (vdotn >= 0.0) {
+		  
+		if (_rho_tot[_qp] > _rho_tot_tol) {
+			
+          // positive gnd exits from element
+          r += ((vdotn * rho_gnd_pos * _u[_qp]) / _rho_tot[_qp]) 
+		       * _test[_i][_qp];
+			   
+		}
+		 
+        if (_rho_tot_neighbor[_qp] > _rho_tot_tol) {
+		
+          // negative gnd enters from neighbour 		
+		  r -= ((vdotn * neigh_rho_gnd_neg * _u_neighbor[_qp]) / _rho_tot_neighbor[_qp]) 
+		       * _test[_i][_qp];
+		
+		}
+		
+	  }
+
+      if (vdotn < 0.0) {
+		  
+        if (_rho_tot_neighbor[_qp] > _rho_tot_tol) {
+			
+		  // positive gnd enters from neighbour
+	      r += ((vdotn * neigh_rho_gnd_pos * _u_neighbor[_qp]) / _rho_tot_neighbor[_qp]) 
+	           * _test[_i][_qp];		
+			
+		}
+
+        if (_rho_tot[_qp] > _rho_tot_tol) {
+			
+          // negative gnd exits from element
+          r -= ((vdotn * rho_gnd_neg * _u[_qp]) / _rho_tot[_qp]) 
+	           * _test[_i][_qp];				
+			
+		}			 
+
+	  }
+		  
+      break;
+
+    case Moose::Neighbor:
+	  		  
+	  if (vdotn >= 0.0) {
+		  
+		if (_rho_tot[_qp] > _rho_tot_tol) {
+			
+          // positive gnd exits from element
+          r -= ((vdotn * rho_gnd_pos * _u[_qp]) / _rho_tot[_qp]) 
+		       * _test_neighbor[_i][_qp];
+			   
+		}
+		 
+        if (_rho_tot_neighbor[_qp] > _rho_tot_tol) {
+		
+          // negative gnd enters from neighbour		
+		  r += ((vdotn * neigh_rho_gnd_neg * _u_neighbor[_qp]) / _rho_tot_neighbor[_qp]) 
+		       * _test_neighbor[_i][_qp];
+		
+		}
+		
+	  }
+
+      if (vdotn < 0.0) {
+		  
+        if (_rho_tot_neighbor[_qp] > _rho_tot_tol) {
+			
+		  // positive gnd enters from neighbour
+	      r -= ((vdotn * neigh_rho_gnd_pos * _u_neighbor[_qp]) / _rho_tot_neighbor[_qp]) 
+	           * _test_neighbor[_i][_qp];		
+			
+		}
+
+        if (_rho_tot[_qp] > _rho_tot_tol) {
+			
+          // negative gnd exits from element
+          r += ((vdotn * rho_gnd_neg * _u[_qp]) / _rho_tot[_qp]) 
+	           * _test_neighbor[_i][_qp];			
+			
+		}			 
+
+	  }
+		  
+      break;
+  }
+
+  return r;
+}
+
+// Exact form of the in-diagonal Jacobian considering the signed gnd densities
+Real 
+DGCurvatureAdvectionPN::computeQpJacobian(Moose::DGJacobianType type) 
+{ 
+  Real jac = 0; // Jacobian for output
+  Real rho_gnd_pos; // positive GND density in this element
+  Real rho_gnd_neg; // negative GND density in this element
+  Real neigh_rho_gnd_pos; // positive GND density in the neighbouring element
+  Real neigh_rho_gnd_neg; // negative GND density in the neighbouring element
+  
+  getDislocationVelocity();
+  
+  Real vdotn = _velocity * _normals[_qp];
+  
+  // Define positive and negative GND densities
+  // both are positive quantities
+  // _u[_qp] is q_tot in this case
+  // _rho_tot[_qp] is rho_tot in this case
+  // _rho_gnd[_qp] is edge or screw, respectively
+  // _rho_gnd_ot[_qp] is screw or edge, respectively
+  // rho_t = sqrt(rho_e^2 + rho_s^2) in the pure GND case
+  
+  rho_gnd_pos = 0.5 * (std::sqrt(_rho_tot[_qp]*_rho_tot[_qp] 
+	          - _rho_gnd_ot[_qp]*_rho_gnd_ot[_qp]) + _rho_gnd[_qp]);
+				  
+  rho_gnd_neg = 0.5 * (std::sqrt(_rho_tot[_qp]*_rho_tot[_qp] 
+	          - _rho_gnd_ot[_qp]*_rho_gnd_ot[_qp]) - _rho_gnd[_qp]);
+				  
+  neigh_rho_gnd_pos = 0.5 * (std::sqrt(_rho_tot_neighbor[_qp]*_rho_tot_neighbor[_qp]
+	                - _rho_gnd_ot_neighbor[_qp]*_rho_gnd_ot_neighbor[_qp]) 
+				    + _rho_gnd_neighbor[_qp]);
+						
+  neigh_rho_gnd_neg = 0.5 * (std::sqrt(_rho_tot_neighbor[_qp]*_rho_tot_neighbor[_qp]
+	                - _rho_gnd_ot_neighbor[_qp]*_rho_gnd_ot_neighbor[_qp]) 
+	                - _rho_gnd_neighbor[_qp]);
+					
+  switch (type)
+  {
+    case Moose::ElementElement:
+	
+      if (vdotn >= 0.0) {
+		  
+		if (_rho_tot[_qp] > _rho_tot_tol) {
+			
+          // positive gnd exits from element
+          jac += ((vdotn * rho_gnd_pos * _phi[_j][_qp]) / _rho_tot[_qp]) 
+		         * _test[_i][_qp];
+			   
+		}		  
+		  
+	  }	
+	
+	  if (vdotn < 0.0) {
+		  
+        if (_rho_tot[_qp] > _rho_tot_tol) {
+			
+          // negative gnd exits from element
+          jac -= ((vdotn * rho_gnd_neg * _phi[_j][_qp]) / _rho_tot[_qp]) 
+	             * _test[_i][_qp];				
+			
+		}			  
+		  
+	  }	
+		  
+      break;
+
+    case Moose::ElementNeighbor:
+	
+	  if (vdotn >= 0.0) {
+		  
+        if (_rho_tot_neighbor[_qp] > _rho_tot_tol) {
+		
+          // negative gnd enters from neighbour 		
+		  jac -= ((vdotn * neigh_rho_gnd_neg * _phi_neighbor[_j][_qp]) / _rho_tot_neighbor[_qp]) 
+		         * _test[_i][_qp];
+		
+		}		  
+		  
+	  }
+	
+	  if (vdotn < 0.0) {
+		  
+        if (_rho_tot_neighbor[_qp] > _rho_tot_tol) {
+			
+		  // positive gnd enters from neighbour
+	      jac += ((vdotn * neigh_rho_gnd_pos * _phi_neighbor[_j][_qp]) / _rho_tot_neighbor[_qp]) 
+	             * _test[_i][_qp];		
+			
+		}		  
+		  
+	  }
+		  
+      break;
+	  
+	case Moose::NeighborElement:
+	
+	  if (vdotn >= 0.0) {
+		  
+		if (_rho_tot[_qp] > _rho_tot_tol) {
+			
+          // positive gnd exits from element
+          jac -= ((vdotn * rho_gnd_pos * _phi[_j][_qp]) / _rho_tot[_qp]) 
+		         * _test_neighbor[_i][_qp];
+			   
+		}		  
+		  
+	  }
+	
+	  if (vdotn < 0.0) {
+		  
+        if (_rho_tot[_qp] > _rho_tot_tol) {
+			
+          // negative gnd exits from element
+          jac += ((vdotn * rho_gnd_neg * _phi[_j][_qp]) / _rho_tot[_qp]) 
+	             * _test_neighbor[_i][_qp];			
+			
+		}		  
+		  
+	  }
+
+	  break;
+	
+	case Moose::NeighborNeighbor:
+	
+      if (vdotn >= 0.0) {
+		  
+        if (_rho_tot_neighbor[_qp] > _rho_tot_tol) {
+		
+          // negative gnd enters from neighbour		
+		  jac += ((vdotn * neigh_rho_gnd_neg * _phi_neighbor[_j][_qp]) / _rho_tot_neighbor[_qp]) 
+		         * _test_neighbor[_i][_qp];
+		
+		}		  
+		  
+	  }	
+	
+	  if (vdotn < 0.0) {
+		  
+        if (_rho_tot_neighbor[_qp] > _rho_tot_tol) {
+			
+		  // positive gnd enters from neighbour
+	      jac -= ((vdotn * neigh_rho_gnd_pos * _phi_neighbor[_j][_qp]) / _rho_tot_neighbor[_qp]) 
+	             * _test_neighbor[_i][_qp];		
+			
+		}
+		  
+	  }
+	
+	  break;
+  }  
+
+  return jac; 
+}
+
+// An approximated form of the non-diagonal Jacobian
+// with the approximation _rho_coupled_ot[_qp] = _rho_coupled_ot_neighbor[_qp] = 0
+Real
+DGCurvatureAdvectionPN::computeQpOffDiagJacobian(Moose::DGJacobianType type, unsigned int jvar)
+{
+  Real jac = 0; // Jacobian for output
+  
+  getDislocationVelocity();
+  
+  Real vdotn = _velocity * _normals[_qp];
+  
+  Real rho_gnd_vdotn = vdotn * _rho_gnd[_qp];
+  Real neigh_rho_gnd_vdotn = vdotn * _rho_gnd_neighbor[_qp];
+  
+  if (_rho_gnd_coupled && jvar == _rho_gnd_var) {
+	  
+    // derivative with respect to _rho_gnd and _rho_gnd_neighbor
+	
+	switch (type)
+    {
+      case Moose::ElementElement:
+	  
+	    if (rho_gnd_vdotn >= 0.0 && _rho_tot[_qp] > _rho_tot_tol)
+          jac += ((_phi[_j][_qp] * _u[_qp]) / _rho_tot[_qp]) 
+	             * _test[_i][_qp];	  
+		
+        break;
+
+      case Moose::ElementNeighbor:
+	  
+        if (neigh_rho_gnd_vdotn < 0.0 && _rho_tot_neighbor[_qp] > _rho_tot_tol)
+	      jac += ((_phi_neighbor[_j][_qp] * _u_neighbor[_qp]) / _rho_tot_neighbor[_qp]) 
+	             * _test[_i][_qp];	  
+		
+        break;	  
+	  
+	  case Moose::NeighborElement:
+	  
+	    if (rho_gnd_vdotn >= 0.0 && _rho_tot[_qp] > _rho_tot_tol)
+          jac -= ((_phi[_j][_qp] * _u[_qp]) / _rho_tot[_qp]) 
+	           * _test_neighbor[_i][_qp];	  
+		
+        break;
+
+	  case Moose::NeighborNeighbor:
+	  
+	    if (neigh_rho_gnd_vdotn < 0.0 && _rho_tot_neighbor[_qp] > _rho_tot_tol)
+	      jac -= ((_phi_neighbor[_j][_qp] * _u_neighbor[_qp]) / _rho_tot_neighbor[_qp])  
+	           * _test_neighbor[_i][_qp];	  
+		
+        break;	  
+	}
+  
+  } else if (_rho_tot_coupled && jvar == _rho_tot_var) {
+	  
+    // derivative with respect to _rho_tot and _rho_tot_neighbor
+	switch (type)
+    {
+	  case Moose::ElementElement:
+	  
+	    if (rho_gnd_vdotn >= 0.0 && _rho_tot[_qp] > _rho_tot_tol)
+          jac += (-1.0) * ((_rho_gnd[_qp] * _u[_qp]) / (_rho_tot[_qp] * _rho_tot[_qp])) 
+	             * _phi[_j][_qp] * _test[_i][_qp];	  
+	  
+	    break;
+		
+	  case Moose::ElementNeighbor:
+	  
+        if (neigh_rho_gnd_vdotn < 0.0 && _rho_tot_neighbor[_qp] > _rho_tot_tol)
+	      jac += (-1.0) * ((_rho_gnd_neighbor[_qp] * _u_neighbor[_qp]) 
+	             / (_rho_tot_neighbor[_qp] * _rho_tot_neighbor[_qp])) 
+	             * _phi_neighbor[_j][_qp] * _test[_i][_qp];		  
+	  
+	    break;
+		
+	  case Moose::NeighborElement:
+	  
+	    if (rho_gnd_vdotn >= 0.0 && _rho_tot[_qp] > _rho_tot_tol)
+          jac -= (-1.0) * ((_rho_gnd[_qp] * _u[_qp]) / (_rho_tot[_qp] * _rho_tot[_qp])) 
+	             * _phi[_j][_qp] * _test_neighbor[_i][_qp];	  
+	  
+	    break;
+		
+	  case Moose::NeighborNeighbor:	
+	  
+	    if (neigh_rho_gnd_vdotn < 0.0 && _rho_tot_neighbor[_qp] > _rho_tot_tol)
+	      jac -= (-1.0) * ((_rho_gnd_neighbor[_qp] * _u_neighbor[_qp]) 
+	             / (_rho_tot_neighbor[_qp] * _rho_tot_neighbor[_qp]))  
+	             * _phi_neighbor[_j][_qp] * _test_neighbor[_i][_qp];	  
+	  
+	    break;
+	}
+	
+  } else {
+	  
+    jac = 0.0;  
+	  
+  }
+  
+  return jac; 
+}
