eff565f5a19f4b7011e97b8fb1de569c3e644c3c
ngrilli <ngrilli@purdue.edu>
2021-10-24 00:16:54 +0100

    CP damage coupling with volumetric and isochoric decomposition

---
+++ include/materials/FiniteStrainUObasedCPDamageVol.h
@@ -0,0 +1,96 @@
+// Nicolò Grilli
+// University of Bristol
+// Aayush Trivedi
+// Alankar Alankar
+// IIT Bombay
+// 23 Ottobre 2021
+
+#pragma once
+
+#include "FiniteStrainUObasedCP.h"
+
+// #include "CrystalPlasticitySlipRate.h"
+// #include "CrystalPlasticitySlipResistance.h"
+// #include "CrystalPlasticityStateVariable.h"
+// #include "CrystalPlasticityStateVarRateComponent.h"
+
+/**
+ * FiniteStrainUObasedCPDamage is the coupling between
+ * crystal plasticity and damage
+ * Free energy is decomposed into volumetric and non-volumetric parts as in:
+ * Nicolo Grilli and Marisol Koslowski
+ * The effect of crystal anisotropy and plastic response 
+ * on the dynamic fracture of energetic materials
+ * Journal of Applied Physics 126, 155101 (2019)
+ */
+class FiniteStrainUObasedCPDamageVol : public FiniteStrainUObasedCP
+{
+public:
+  static InputParameters validParams();
+
+  FiniteStrainUObasedCPDamageVol(const InputParameters & parameters);
+
+protected:
+  /**
+   * calculate stress residual.
+   * Damage is added to the stress calculation
+   */
+  virtual void calcResidual();
+  
+  /**
+   * Method to split elastic energy based on strain volumetric/non-volumetric decomposition
+   * @param F_pos tensile part of total elastic energy
+   * @param F_neg compressive part of total elastic energy
+   * @param ee elastic Green-Lagrange strain
+   * @param ce right Cauchy–Green deformation tensor (elastic)
+   * @param pk2_new second Piola-Kirchhoff stress
+   */
+  virtual void computeStrainVolumetric(Real & F_pos, Real & F_neg, 
+                                       RankTwoTensor & ee, RankTwoTensor & ce, 
+									   RankTwoTensor & pk2_new);
+
+  // compute history variable and assign to _E
+  // which is used by the fracture model for damage growth
+  // Damage grows only because of the positive part of the elastic energy F_pos									 
+  virtual void computeHistoryVariable(Real & F_pos, Real & F_neg);
+
+  /// Variable defining the phase field damage parameter
+  const VariableValue & _c;
+  
+  /// Use current value of history variable
+  bool _use_current_hist;
+  
+  /// History variable that prevents crack healing, declared in this material
+  MaterialProperty<Real> & _H;
+
+  /// Old value of history variable
+  const MaterialProperty<Real> & _H_old;
+  
+  /// Material property for elastic energy
+  MaterialProperty<Real> & _E;
+
+  /// Derivative of elastic energy w.r.t damage variable
+  MaterialProperty<Real> & _dEdc;
+
+  /// Second-order derivative of elastic energy w.r.t damage variable
+  MaterialProperty<Real> & _d2Ed2c;
+  
+  /// Derivative of stress w.r.t damage variable
+  MaterialProperty<RankTwoTensor> & _dstress_dc;
+  
+  /// Second-order derivative of elastic energy w.r.t damage variable and strain
+  MaterialProperty<RankTwoTensor> & _d2Fdcdstrain;
+  
+  /// Material property for energetic degradation function
+  /// for instance, (1-c)^2 can be used
+  const MaterialProperty<Real> & _D;
+
+  /// Derivative of degradation function w.r.t damage variable
+  const MaterialProperty<Real> & _dDdc;
+
+  /// Second-order derivative of degradation w.r.t damage variable
+  const MaterialProperty<Real> & _d2Dd2c;
+  
+  const Real _bulk_modulus_ref; // reference bulk modulus for vol/non-vol decomposition
+
+};
---
+++ src/materials/FiniteStrainUObasedCPDamageVol.C
@@ -0,0 +1,295 @@
+// Nicolò Grilli
+// University of Bristol
+// Aayush Trivedi
+// Alankar Alankar
+// IIT Bombay
+// 23 Ottobre 2021
+
+#include "FiniteStrainUObasedCPDamageVol.h"
+
+#include "petscblaslapack.h"
+#include "MooseException.h"
+// #include "CrystalPlasticitySlipRate.h"
+// #include "CrystalPlasticitySlipResistance.h"
+// #include "CrystalPlasticityStateVariable.h"
+// #include "CrystalPlasticityStateVarRateComponent.h"
+#include "MathUtils.h"
+
+registerMooseObject("TensorMechanicsApp", FiniteStrainUObasedCPDamageVol);
+
+InputParameters
+FiniteStrainUObasedCPDamageVol::validParams()
+{
+  InputParameters params = FiniteStrainUObasedCP::validParams();
+  params.addClassDescription("UserObject based Crystal Plasticity system with damage. "
+                             "Free energy is decomposed into volumetric and non-volumetric " 
+							 "parts as in: "
+							 "Nicolo Grilli and Marisol Koslowski "
+							 "The effect of crystal anisotropy and plastic response "
+							 "on the dynamic fracture of energetic materials "
+							 "Journal of Applied Physics 126, 155101 (2019).");
+  params.addRequiredCoupledVar("c", "Order parameter for damage");
+  params.addParam<bool>(
+      "use_current_history_variable", false, "Use the current value of the history variable.");
+  params.addParam<MaterialPropertyName>(
+      "E_name", "elastic_energy", "Name of material property for elastic energy");
+  params.addParam<MaterialPropertyName>(
+      "D_name", "degradation", "Name of material property for energetic degradation function.");
+  params.addParam<Real>("bulk_modulus_ref",0.0,"reference bulk modulus for vol/non-vol decomposition");
+  return params;
+}
+
+FiniteStrainUObasedCPDamageVol::FiniteStrainUObasedCPDamageVol(const InputParameters & parameters)
+  : FiniteStrainUObasedCP(parameters),
+    _c(coupledValue("c")),
+    _use_current_hist(getParam<bool>("use_current_history_variable")),
+    _H(declareProperty<Real>("hist")), // History variable to avoid damage decrease 
+    _H_old(getMaterialPropertyOld<Real>("hist")),
+    _E(declareProperty<Real>(getParam<MaterialPropertyName>("E_name"))),
+    _dEdc(declarePropertyDerivative<Real>(getParam<MaterialPropertyName>("E_name"),
+                                          getVar("c", 0)->name())),
+    _d2Ed2c(declarePropertyDerivative<Real>(
+        getParam<MaterialPropertyName>("E_name"), getVar("c", 0)->name(), getVar("c", 0)->name())),
+    _dstress_dc(
+        declarePropertyDerivative<RankTwoTensor>(_base_name + "stress", getVar("c", 0)->name())), 
+    _d2Fdcdstrain(declareProperty<RankTwoTensor>("d2Fdcdstrain")),		
+	_D(getMaterialProperty<Real>("D_name")),
+    _dDdc(getMaterialPropertyDerivative<Real>("D_name", getVar("c", 0)->name())),
+    _d2Dd2c(getMaterialPropertyDerivative<Real>(
+        "D_name", getVar("c", 0)->name(), getVar("c", 0)->name())),
+    _bulk_modulus_ref(getParam<Real>("bulk_modulus_ref")) // reference bulk modulus for vol/non-vol decomposition
+{
+  _err_tol = false;
+
+  _delta_dfgrd.zero();
+
+  // resize the material properties for each userobject
+  _mat_prop_slip_rates.resize(_num_uo_slip_rates);
+  _mat_prop_slip_resistances.resize(_num_uo_slip_resistances);
+  _mat_prop_state_vars.resize(_num_uo_state_vars);
+  _mat_prop_state_vars_old.resize(_num_uo_state_vars);
+  _mat_prop_state_var_evol_rate_comps.resize(_num_uo_state_var_evol_rate_comps);
+
+  // resize the flow direction
+  _flow_direction.resize(_num_uo_slip_rates);
+
+  // resize local state variables
+  _state_vars_old.resize(_num_uo_state_vars);
+  _state_vars_old_stored.resize(_num_uo_state_vars);
+  _state_vars_prev.resize(_num_uo_state_vars);
+
+  // resize user objects
+  _uo_slip_rates.resize(_num_uo_slip_rates);
+  _uo_slip_resistances.resize(_num_uo_slip_resistances);
+  _uo_state_vars.resize(_num_uo_state_vars);
+  _uo_state_var_evol_rate_comps.resize(_num_uo_state_var_evol_rate_comps);
+
+  // assign the user objects
+  for (unsigned int i = 0; i < _num_uo_slip_rates; ++i)
+  {
+    _uo_slip_rates[i] = &getUserObjectByName<CrystalPlasticitySlipRate>(
+        parameters.get<std::vector<UserObjectName>>("uo_slip_rates")[i]);
+    _mat_prop_slip_rates[i] = &declareProperty<std::vector<Real>>(
+        parameters.get<std::vector<UserObjectName>>("uo_slip_rates")[i]);
+    _flow_direction[i] = &declareProperty<std::vector<RankTwoTensor>>(
+        parameters.get<std::vector<UserObjectName>>("uo_slip_rates")[i] + "_flow_direction");
+  }
+
+  for (unsigned int i = 0; i < _num_uo_slip_resistances; ++i)
+  {
+    _uo_slip_resistances[i] = &getUserObjectByName<CrystalPlasticitySlipResistance>(
+        parameters.get<std::vector<UserObjectName>>("uo_slip_resistances")[i]);
+    _mat_prop_slip_resistances[i] = &declareProperty<std::vector<Real>>(
+        parameters.get<std::vector<UserObjectName>>("uo_slip_resistances")[i]);
+  }
+
+  for (unsigned int i = 0; i < _num_uo_state_vars; ++i)
+  {
+    _uo_state_vars[i] = &getUserObjectByName<CrystalPlasticityStateVariable>(
+        parameters.get<std::vector<UserObjectName>>("uo_state_vars")[i]);
+    _mat_prop_state_vars[i] = &declareProperty<std::vector<Real>>(
+        parameters.get<std::vector<UserObjectName>>("uo_state_vars")[i]);
+    _mat_prop_state_vars_old[i] = &getMaterialPropertyOld<std::vector<Real>>(
+        parameters.get<std::vector<UserObjectName>>("uo_state_vars")[i]);
+  }
+
+  for (unsigned int i = 0; i < _num_uo_state_var_evol_rate_comps; ++i)
+  {
+    _uo_state_var_evol_rate_comps[i] = &getUserObjectByName<CrystalPlasticityStateVarRateComponent>(
+        parameters.get<std::vector<UserObjectName>>("uo_state_var_evol_rate_comps")[i]);
+    _mat_prop_state_var_evol_rate_comps[i] = &declareProperty<std::vector<Real>>(
+        parameters.get<std::vector<UserObjectName>>("uo_state_var_evol_rate_comps")[i]);
+  }
+
+  _substep_dt = 0.0;
+}
+
+void
+FiniteStrainUObasedCPDamageVol::calcResidual()
+{
+  RankTwoTensor iden(RankTwoTensor::initIdentity), ce, ee, ce_pk2, eqv_slip_incr, pk2_new;
+  
+  Real F_pos, F_neg; // tensile and compressive part of the elastic strain energy
+
+  getSlipRates();
+  if (_err_tol)
+    return;
+
+  for (unsigned int i = 0; i < _num_uo_slip_rates; ++i)
+    for (unsigned int j = 0; j < _uo_slip_rates[i]->variableSize(); ++j)
+      eqv_slip_incr +=
+          (*_flow_direction[i])[_qp][j] * (*_mat_prop_slip_rates[i])[_qp][j] * _substep_dt;
+
+  eqv_slip_incr = iden - eqv_slip_incr;
+  _fp_inv = _fp_old_inv * eqv_slip_incr;
+  _fe = _dfgrd_tmp * _fp_inv;
+
+  ce = _fe.transpose() * _fe;
+  ee = ce - iden;
+  ee *= 0.5;
+  
+  // Decompose ee into volumetric and non-volumetric
+  // and calculate elastic energy and stress
+  computeStrainVolumetric(F_pos, F_neg, ee, ce, pk2_new);
+  
+  // calculate history variable and
+  // assign elastic free energy to _E
+  // for the fracture model
+  computeHistoryVariable(F_pos, F_neg);
+
+  // Anisotropic undamaged
+  // pk2_new = _elasticity_tensor[_qp] * ee;
+
+  _resid = _pk2[_qp] - pk2_new;
+}
+
+void
+FiniteStrainUObasedCPDamageVol::computeStrainVolumetric(Real & F_pos, Real & F_neg, 
+                                                        RankTwoTensor & ee, RankTwoTensor & ce, 
+														RankTwoTensor & pk2_new)
+{
+  //Anisotropic elasticity
+  Real Kb = 0.0; // Kb is the reference bulk modulus
+  Real Je; // Je is relative elastic volume change
+  Real Je23; // Je^{2/3}
+  Real delta; // delta is the trace of volumetric part of elastic Green-Lagrange strain
+  
+  // Positive and negative volumetric free energies
+  Real a_pos_vol, a_neg_vol;
+  
+  // Isochoric free energy
+  Real a_pos_cpl;
+  
+  // Undamaged elastic energy tensor
+  RankTwoTensor undamaged_elastic_energy;
+  
+  // Positive and negative part of the Piola-Kirchhoff stress
+  RankTwoTensor pk2_pos;
+  RankTwoTensor pk2_neg;
+  
+  // inverse of the right Cauchy–Green deformation tensor (elastic)
+  RankTwoTensor invce;
+  
+  // Anisotropic elasticity (Luscher 2017)
+  // Kb = K in (Luscher 2017)
+  // Kb = (1/9) I : C : I
+  for (unsigned int i = 0; i < LIBMESH_DIM; ++i)
+    for (unsigned int j = 0; j < LIBMESH_DIM; ++j)
+      Kb += _elasticity_tensor[_qp](i, i, j, j);
+  
+  Kb = Kb / 9.0;
+
+  Je = _fe.det();
+  Je23 = std::pow(Je,2.0/3.0);
+  delta = 1.5 * (Je23 - 1.0);
+  
+  // Calculate volumetric free energy
+  // Equations 15 and 16 in Grilli, Koslowski, 2019
+  
+  if (Je >= 1.0) { // expansion
+	  
+    a_pos_vol = 0.5 * Kb * delta * delta;
+    a_neg_vol = 0.0;
+	  
+  } else { // compression
+	  
+    a_pos_vol = 0.0;
+	a_neg_vol = 0.5 * Kb * delta * delta;
+	
+  }
+  
+  // Calculate isochoric free energy
+  // Equation 17 in Grilli, Koslowski, 2019
+
+  undamaged_elastic_energy = _elasticity_tensor[_qp] * ee;
+  undamaged_elastic_energy = 0.5 * ee * undamaged_elastic_energy;
+
+  a_pos_cpl = undamaged_elastic_energy.trace();
+  a_pos_cpl -= 0.5 * Kb * delta * delta;
+
+  // Calculate positive and negative parts of the Piola-Kirchhoff stress
+  // Equation 18 in Grilli, Koslowski, 2019
+  if (Je >= 1.0) { // expansion
+  
+    pk2_pos = _elasticity_tensor[_qp] * ee;
+    pk2_neg = 0.0;
+	
+  } else { // compression
+  
+    invce = ce.inverse();
+	
+	pk2_neg = Je23 * Kb * delta * invce;
+	
+    pk2_pos = (-1.0) * pk2_neg;	
+    pk2_pos += _elasticity_tensor[_qp] * ee;
+
+  }
+
+  // Positive part of the stress is degraded by _D[_qp]
+  pk2_new = (_D[_qp] * pk2_pos) + pk2_neg;
+  
+  // Positive and negative parts of the free energy
+  // Equations 13 and 14 in Grilli, Koslowski, 2019
+  F_pos = a_pos_vol + a_pos_cpl;
+  F_neg = a_neg_vol;
+  
+  // Used in StressDivergencePFFracTensors off-diagonal Jacobian
+  _dstress_dc[_qp] = pk2_pos * _dDdc[_qp];
+  
+  // 2nd derivative wrt c and strain = 0.0 if we used the previous step's history variable
+  if (_use_current_hist)
+    _d2Fdcdstrain[_qp] = pk2_pos * _dDdc[_qp];
+  
+  // _Jacobian_mult is already defined in the CP base class
+  
+}
+
+// compute history variable and assign to _E
+// which is used by the fracture model for damage growth
+// Damage grows only because of the positive part of the elastic energy F_pos
+void
+FiniteStrainUObasedCPDamageVol::computeHistoryVariable(Real & F_pos, Real & F_neg)
+{
+  // Assign history variable
+  Real hist_variable = _H_old[_qp];
+  
+  // _use_snes_vi_solver option not implemented
+
+  if (F_pos > _H_old[_qp])
+    _H[_qp] = F_pos;
+  else
+    _H[_qp] = _H_old[_qp];
+
+  if (_use_current_hist)
+    hist_variable = _H[_qp];
+
+  // _barrier not implemented
+
+  // Elastic free energy density and derivatives
+  _E[_qp] = hist_variable * _D[_qp] + F_neg;
+  _dEdc[_qp] = hist_variable * _dDdc[_qp];
+  _d2Ed2c[_qp] = hist_variable * _d2Dd2c[_qp];
+
+}
+
+
