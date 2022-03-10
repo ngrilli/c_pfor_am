53fa472ea5d9bca8cc1f05d66ede70663995c862
ngrilli <ngrilli@purdue.edu>
2022-03-10 19:17:23 +0000

    User Object for Backstress

---
+++ include/userobjects/CrystalPlasticityStateVarRateBackstress.h
@@ -0,0 +1,34 @@
+// Aayush Trivedi
+// Alankar Alankar
+// IIT Bombay
+// Nicolò Grilli
+// University of Bristol
+// 10 Marzo 2022
+
+#pragma once
+
+#include "CrystalPlasticityStateVarRateComponent.h"
+
+/**
+ * Evolution rate of the backstress based on Armstrong-Frederick.
+ */
+class CrystalPlasticityStateVarRateBackstress : public CrystalPlasticityStateVarRateComponent
+{
+public:
+  static InputParameters validParams();
+
+  CrystalPlasticityStateVarRateBackstress(const InputParameters & parameters);
+
+  virtual bool calcStateVariableEvolutionRateComponent(unsigned int qp,
+                                                       std::vector<Real> & val) const;
+
+protected:
+
+  const MaterialProperty<std::vector<Real>> & _mat_prop_slip_rate;
+
+  const MaterialProperty<std::vector<Real>> & _mat_prop_backstress;
+
+  // The backstress parameters read from .i file
+  std::vector<Real> _bprops;
+
+};
---
+++ src/userobjects/CrystalPlasticityStateVarRateBackstress.C
@@ -0,0 +1,58 @@
+// Aayush Trivedi
+// Alankar Alankar
+// IIT Bombay
+// Nicolò Grilli
+// University of Bristol
+// 10 Marzo 2022
+
+#include "CrystalPlasticityStateVarRateBackstress.h"
+#include <cmath>
+
+#include <fstream>
+
+registerMooseObject("TensorMechanicsApp", CrystalPlasticityStateVarRateBackstress);
+
+InputParameters
+CrystalPlasticityStateVarRateBackstress::validParams()
+{
+  InputParameters params = CrystalPlasticityStateVarRateComponent::validParams();
+  params.addClassDescription("Evolution rate of the backstress based on Armstrong-Frederick. ");  
+  params.addParam<std::string>(
+      "uo_slip_rate_name",
+      "Name of slip rate property: Same as slip rate user object specified in input file.");
+  params.addParam<std::string>("uo_backstress_var_name",
+                               "Name of backstress variable property: Same as "
+                               "backstress variable user object specified in input file. ");
+  // Reading the two backstress parameters from file is not implemented
+  params.addParam<std::vector<Real>>("bprops","Two parameters required to calculate backstress");
+  return params;
+}
+
+CrystalPlasticityStateVarRateBackstress::CrystalPlasticityStateVarRateBackstress(const InputParameters & parameters)
+  : CrystalPlasticityStateVarRateComponent(parameters),
+    _mat_prop_slip_rate(
+      getMaterialProperty<std::vector<Real>>(parameters.get<std::string>("uo_slip_rate_name"))),
+    _mat_prop_backstress(
+      getMaterialProperty<std::vector<Real>>(parameters.get<std::string>("uo_backstress_var_name"))),
+    _bprops(getParam<std::vector<Real>>("bprops"))
+{
+}
+
+bool
+CrystalPlasticityStateVarRateBackstress::calcStateVariableEvolutionRateComponent(
+    unsigned int qp, std::vector<Real> & val) const
+{
+  val.assign(_variable_size, 0.0);
+  
+  // Backstress parameters of the Armstrong-Frederick law
+  Real ha = _bprops[0];
+  Real hd = _bprops[1];
+
+  for (unsigned int i = 0; i < _variable_size; ++i)
+  {
+    val[i] = ha * _mat_prop_slip_rate[qp][i] 
+	       - hd * _mat_prop_backstress[qp][i] * std::abs(_mat_prop_slip_rate[qp][i]);
+  }
+
+  return true;
+}
