198c5d3578645a1844f6b7d24fa8f9c86f099bf5
ngrilli <ngrilli@purdue.edu>
2022-03-27 03:05:48 +0100

    edge slip direction stored in the update stress CP code

---
+++ include/materials/CrystalPlasticityDislocationUpdate.h
@@ -0,0 +1,156 @@
+// Nicolò Grilli
+// University of Bristol
+// 27 Marzo 2022
+
+#pragma once
+
+#include "CrystalPlasticityStressUpdateBase.h"
+
+class CrystalPlasticityDislocationUpdate;
+
+/**
+ * CrystalPlasticityDislocationUpdate uses the multiplicative decomposition of the
+ * deformation gradient and solves the PK2 stress residual equation at the
+ * intermediate configuration to evolve the material state. The internal
+ * variables are updated using an interative predictor-corrector algorithm.
+ * Backward Euler integration rule is used for the rate equations.
+ * Dislocation based model for crystal plasticity.
+ */
+
+class CrystalPlasticityDislocationUpdate : public CrystalPlasticityStressUpdateBase
+{
+public:
+  static InputParameters validParams();
+
+  CrystalPlasticityDislocationUpdate(const InputParameters & parameters);
+
+protected:
+  /**
+   * initializes the stateful properties such as
+   * stress, plastic deformation gradient, slip system resistances, etc.
+   */
+  virtual void initQpStatefulProperties() override;
+  
+  /**
+   * A helper method to rotate the a direction and plane normal system set into
+   * the local crystal lattice orientation as defined by the crystal rotation
+   * tensor from the Elasticity tensor class.
+   * Edge and screw slip directions are also assigned
+   */
+  void calculateSchmidTensor(const unsigned int & number_dislocation_systems,
+                             const std::vector<RealVectorValue> & plane_normal_vector,
+                             const std::vector<RealVectorValue> & direction_vector,
+                             std::vector<RankTwoTensor> & schmid_tensor,
+                             const RankTwoTensor & crysrot);
+
+
+  /**
+   * Sets the value of the current and previous substep iteration slip system
+   * resistance to the old value at the start of the PK2 stress convergence
+   * while loop.
+   */
+  virtual void setInitialConstitutiveVariableValues() override;
+
+  /**
+   * Sets the current slip system resistance value to the previous substep value.
+   * In cases where only one substep is taken (or when the first) substep is taken,
+   * this method just sets the current value to the old slip system resistance
+   * value again.
+   */
+  virtual void setSubstepConstitutiveVariableValues() override;
+
+  /**
+   * Stores the current value of the slip system resistance into a separate
+   * material property in case substepping is needed.
+   */
+  virtual void updateSubstepConstitutiveVariableValues() override;
+
+  virtual bool calculateSlipRate() override;
+
+  virtual void
+  calculateEquivalentSlipIncrement(RankTwoTensor & /*equivalent_slip_increment*/) override;
+
+  virtual void calculateConstitutiveSlipDerivative(std::vector<Real> & dslip_dtau) override;
+
+  // Cache the slip system value before the update for the diff in the convergence check
+  virtual void cacheStateVariablesBeforeUpdate() override;
+
+  /**
+   * Following the Constitutive model for slip system resistance as given in
+   * Dislocation based model: add reference to do ...
+   */
+  virtual void calculateStateVariableEvolutionRateComponent() override;
+
+  /*
+   * Finalizes the values of the state variables and slip system resistance
+   * for the current timestep after convergence has been reached.
+   */
+  virtual bool updateStateVariables() override;
+
+  /*
+   * Determines if the state variables, e.g. defect densities, have converged
+   * by comparing the change in the values over the iteration period.
+   */
+  virtual bool areConstitutiveStateVariablesConverged() override;
+
+  ///@{Varibles used in the Kalidindi 1992 slip system resistance constiutive model
+  const Real _r;
+  const Real _h;
+  const Real _tau_sat;
+  const Real _gss_a;
+  const Real _ao;
+  const Real _xm;
+  const Real _gss_initial;
+  ///@}
+
+  /**
+   * Slip system interaction matrix used to calculate the hardening contributions
+   * from the self and latent slip systems, from Kalidindi et al (1992).
+   */
+  std::vector<Real> _hb;
+
+  /// Increment of increased resistance for each slip system
+  std::vector<Real> _slip_resistance_increment;
+
+  /**
+   * Stores the values of the slip system resistance from the previous substep
+   * In classes which use dislocation densities, analogous dislocation density
+   * substep vectors will be required.
+   */
+  std::vector<Real> _previous_substep_slip_resistance;
+
+  /**
+   * Caches the value of the current slip system resistance immediately prior
+   * to the update of the slip system resistance, and is used to calculate the
+   * the slip system resistance increment for the current substep (or step if
+   * only one substep is taken) for the convergence check tolerance comparison.
+   * In classes which use dislocation densities, analogous dislocation density
+   * caching vectors will also be required.
+   */
+  std::vector<Real> _slip_resistance_before_update;
+
+  /**
+   * Flag to include the total twin volume fraction in the plastic velocity
+   * gradient calculation, per Kalidindi IJP (2001).
+   */
+  const bool _include_twinning_in_Lp;
+
+  /**
+   * User-defined material property name for the total volume fraction of twins
+   * in a twinning propagation constitutive model, when this class is used in
+   * conjunction with the twinning propagation model.
+   * Note that this value is the OLD material property and thus lags the current
+   * value by a single timestep.
+   */
+  const MaterialProperty<Real> * const _twin_volume_fraction_total;
+  
+  // Rotated slip direction to calculate the directional derivative
+  // of the plastic strain rate
+  // it indicates the edge dislocation velocity direction for all slip systems
+  MaterialProperty<std::vector<Real>> & _edge_slip_direction;
+  
+  // edge dislocation line direction
+  // corresponding to direction of motion of screw dislocations
+  MaterialProperty<std::vector<Real>> & _screw_slip_direction;
+  
+};
---
+++ src/materials/CrystalPlasticityDislocationUpdate.C
@@ -0,0 +1,263 @@
+// Nicolò Grilli
+// University of Bristol
+// 27 Marzo 2022
+
+#include "CrystalPlasticityDislocationUpdate.h"
+#include "libmesh/int_range.h"
+
+registerMooseObject("TensorMechanicsApp", CrystalPlasticityDislocationUpdate);
+
+InputParameters
+CrystalPlasticityDislocationUpdate::validParams()
+{
+  InputParameters params = CrystalPlasticityStressUpdateBase::validParams();
+  params.addClassDescription("Dislocation based model for crystal plasticity "
+                             "using the stress update code");
+  params.addParam<Real>("r", 1.0, "Latent hardening coefficient");
+  params.addParam<Real>("h", 541.5, "hardening constants");
+  params.addParam<Real>("t_sat", 109.8, "saturated slip system strength");
+  params.addParam<Real>("gss_a", 2.5, "coefficient for hardening");
+  params.addParam<Real>("ao", 0.001, "slip rate coefficient");
+  params.addParam<Real>("xm", 0.1, "exponent for slip rate");
+  params.addParam<Real>("gss_initial", 60.8, "initial lattice friction strength of the material");
+
+  params.addParam<MaterialPropertyName>(
+      "total_twin_volume_fraction",
+      "Total twin volume fraction, if twinning is considered in the simulation");
+
+  return params;
+}
+
+CrystalPlasticityDislocationUpdate::CrystalPlasticityDislocationUpdate(
+    const InputParameters & parameters)
+  : CrystalPlasticityStressUpdateBase(parameters),
+    // Constitutive values
+    _r(getParam<Real>("r")),
+    _h(getParam<Real>("h")),
+    _tau_sat(getParam<Real>("t_sat")),
+    _gss_a(getParam<Real>("gss_a")),
+    _ao(getParam<Real>("ao")),
+    _xm(getParam<Real>("xm")),
+    _gss_initial(getParam<Real>("gss_initial")),
+
+    // resize vectors used in the consititutive slip hardening
+    _hb(_number_slip_systems, 0.0),
+    _slip_resistance_increment(_number_slip_systems, 0.0),
+
+    // resize local caching vectors used for substepping
+    _previous_substep_slip_resistance(_number_slip_systems, 0.0),
+    _slip_resistance_before_update(_number_slip_systems, 0.0),
+
+    // Twinning contributions, if used
+    _include_twinning_in_Lp(parameters.isParamValid("total_twin_volume_fraction")),
+    _twin_volume_fraction_total(_include_twinning_in_Lp
+                                    ? &getMaterialPropertyOld<Real>("total_twin_volume_fraction")
+                                    : nullptr),
+
+    // store edge and screw slip directions to calculate directional derivatives
+    // of the plastic slip rate	
+    _edge_slip_direction(declareProperty<std::vector<Real>>("edge_slip_direction")),
+	_screw_slip_direction(declareProperty<std::vector<Real>>("screw_slip_direction"))
+{
+}
+
+void
+CrystalPlasticityDislocationUpdate::initQpStatefulProperties()
+{
+  CrystalPlasticityStressUpdateBase::initQpStatefulProperties();
+
+  for (const auto i : make_range(_number_slip_systems))
+  {
+    _slip_resistance[_qp][i] = _gss_initial;
+    _slip_increment[_qp][i] = 0.0;
+  }
+}
+
+// Calculate Schmid tensor and
+// store edge and screw slip directions to calculate directional derivatives
+// of the plastic slip rate
+void
+CrystalPlasticityDislocationUpdate::calculateSchmidTensor(
+    const unsigned int & number_slip_systems,
+    const std::vector<RealVectorValue> & plane_normal_vector,
+    const std::vector<RealVectorValue> & direction_vector,
+    std::vector<RankTwoTensor> & schmid_tensor,
+    const RankTwoTensor & crysrot)
+{
+  std::vector<RealVectorValue> local_direction_vector, local_plane_normal;
+  local_direction_vector.resize(number_slip_systems);
+  local_plane_normal.resize(number_slip_systems);
+
+  // Update slip direction and normal with crystal orientation
+  for (const auto i : make_range(_number_slip_systems))
+  {
+    local_direction_vector[i].zero();
+    local_plane_normal[i].zero();
+
+    for (const auto j : make_range(LIBMESH_DIM))
+      for (const auto k : make_range(LIBMESH_DIM))
+      {
+        local_direction_vector[i](j) =
+            local_direction_vector[i](j) + crysrot(j, k) * direction_vector[i](k);
+
+        local_plane_normal[i](j) =
+            local_plane_normal[i](j) + crysrot(j, k) * plane_normal_vector[i](k);
+      }
+
+    // Calculate Schmid tensor
+    for (const auto j : make_range(LIBMESH_DIM))
+      for (const auto k : make_range(LIBMESH_DIM))
+      {
+        schmid_tensor[i](j, k) = local_direction_vector[i](j) * local_plane_normal[i](k);
+      }
+  }
+  
+  // Store edge and screw slip directions are also assigned
+  _edge_slip_direction[_qp].resize(LIBMESH_DIM * _number_slip_systems);
+  
+  for (const auto i : make_range(_number_slip_systems)) {
+	for (const auto j : make_range(LIBMESH_DIM)) {
+	  _edge_slip_direction[_qp][i * LIBMESH_DIM + j] = local_direction_vector[i](j)
+	} 
+  }
+  
+}
+
+void
+CrystalPlasticityDislocationUpdate::setInitialConstitutiveVariableValues()
+{
+  // Would also set old dislocation densities here if included in this model
+  _slip_resistance[_qp] = _slip_resistance_old[_qp];
+  _previous_substep_slip_resistance = _slip_resistance_old[_qp];
+}
+
+void
+CrystalPlasticityDislocationUpdate::setSubstepConstitutiveVariableValues()
+{
+  // Would also set substepped dislocation densities here if included in this model
+  _slip_resistance[_qp] = _previous_substep_slip_resistance;
+}
+
+bool
+CrystalPlasticityDislocationUpdate::calculateSlipRate()
+{
+  for (const auto i : make_range(_number_slip_systems))
+  {
+    _slip_increment[_qp][i] =
+        _ao * std::pow(std::abs(_tau[_qp][i] / _slip_resistance[_qp][i]), 1.0 / _xm);
+    if (_tau[_qp][i] < 0.0)
+      _slip_increment[_qp][i] *= -1.0;
+
+    if (std::abs(_slip_increment[_qp][i]) * _substep_dt > _slip_incr_tol)
+    {
+      if (_print_convergence_message)
+        mooseWarning("Maximum allowable slip increment exceeded ",
+                     std::abs(_slip_increment[_qp][i]) * _substep_dt);
+
+      return false;
+    }
+  }
+  return true;
+}
+
+void
+CrystalPlasticityDislocationUpdate::calculateEquivalentSlipIncrement(
+    RankTwoTensor & equivalent_slip_increment)
+{
+  if (_include_twinning_in_Lp)
+  {
+    for (const auto i : make_range(_number_slip_systems))
+      equivalent_slip_increment += (1.0 - (*_twin_volume_fraction_total)[_qp]) *
+                                   _flow_direction[_qp][i] * _slip_increment[_qp][i] * _substep_dt;
+  }
+  else // if no twinning volume fraction material property supplied, use base class
+    CrystalPlasticityStressUpdateBase::calculateEquivalentSlipIncrement(equivalent_slip_increment);
+}
+
+void
+CrystalPlasticityDislocationUpdate::calculateConstitutiveSlipDerivative(
+    std::vector<Real> & dslip_dtau)
+{
+  for (const auto i : make_range(_number_slip_systems))
+  {
+    if (MooseUtils::absoluteFuzzyEqual(_tau[_qp][i], 0.0))
+      dslip_dtau[i] = 0.0;
+    else
+      dslip_dtau[i] = _ao / _xm *
+                      std::pow(std::abs(_tau[_qp][i] / _slip_resistance[_qp][i]), 1.0 / _xm - 1.0) /
+                      _slip_resistance[_qp][i];
+  }
+}
+
+bool
+CrystalPlasticityDislocationUpdate::areConstitutiveStateVariablesConverged()
+{
+  return isConstitutiveStateVariableConverged(_slip_resistance[_qp],
+                                              _slip_resistance_before_update,
+                                              _previous_substep_slip_resistance,
+                                              _resistance_tol);
+}
+
+void
+CrystalPlasticityDislocationUpdate::updateSubstepConstitutiveVariableValues()
+{
+  // Would also set substepped dislocation densities here if included in this model
+  _previous_substep_slip_resistance = _slip_resistance[_qp];
+}
+
+void
+CrystalPlasticityDislocationUpdate::cacheStateVariablesBeforeUpdate()
+{
+  _slip_resistance_before_update = _slip_resistance[_qp];
+}
+
+void
+CrystalPlasticityDislocationUpdate::calculateStateVariableEvolutionRateComponent()
+{
+  for (const auto i : make_range(_number_slip_systems))
+  {
+    // Clear out increment from the previous iteration
+    _slip_resistance_increment[i] = 0.0;
+
+    _hb[i] = _h * std::pow(std::abs(1.0 - _slip_resistance[_qp][i] / _tau_sat), _gss_a);
+    const Real hsign = 1.0 - _slip_resistance[_qp][i] / _tau_sat;
+    if (hsign < 0.0)
+      _hb[i] *= -1.0;
+  }
+
+  for (const auto i : make_range(_number_slip_systems))
+  {
+    for (const auto j : make_range(_number_slip_systems))
+    {
+      unsigned int iplane, jplane;
+      iplane = i / 3;
+      jplane = j / 3;
+
+      if (iplane == jplane) // self vs. latent hardening
+        _slip_resistance_increment[i] +=
+            std::abs(_slip_increment[_qp][j]) * _hb[j]; // q_{ab} = 1.0 for self hardening
+      else
+        _slip_resistance_increment[i] +=
+            std::abs(_slip_increment[_qp][j]) * _r * _hb[j]; // latent hardenign
+    }
+  }
+}
+
+bool
+CrystalPlasticityDislocationUpdate::updateStateVariables()
+{
+  // Now perform the check to see if the slip system should be updated
+  for (const auto i : make_range(_number_slip_systems))
+  {
+    _slip_resistance_increment[i] *= _substep_dt;
+    if (_previous_substep_slip_resistance[i] < _zero_tol && _slip_resistance_increment[i] < 0.0)
+      _slip_resistance[_qp][i] = _previous_substep_slip_resistance[i];
+    else
+      _slip_resistance[_qp][i] =
+          _previous_substep_slip_resistance[i] + _slip_resistance_increment[i];
+
+    if (_slip_resistance[_qp][i] < 0.0)
+      return false;
+  }
+  return true;
+}
