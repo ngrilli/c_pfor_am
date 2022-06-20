36f196f388310515de6bf36e545350b429d3129c
Nicolo Grilli <nicolo.grilli@bristol.ac.uk>
2022-06-20 17:23:35 +0100

    set residual_def_level variable as required

--- src/materials/ComputeCrystalPlasticityResidualEigenstrain.C
+++ src/materials/ComputeCrystalPlasticityResidualEigenstrain.C
@@ -5,7 +5,7 @@
 // Compute eigenstrain representing residual deformation
 // that induces residual stress in a sample
 // Eigenstrain grows to the maximum value based on a coupled variable
-// called residual_def_level that grows from 0 to 1 
+// called residual_def_level that grows from 0 to 1
 // The maximum residual deformation can be homogeneous in space
 // or can be read from file to apply a non-homogeneous distribution
 // The residual deformation is represented by 9 numbers
@@ -20,19 +20,19 @@ ComputeCrystalPlasticityResidualEigenstrain::validParams()
 {
   InputParameters params = ComputeCrystalPlasticityEigenstrainBase::validParams();
 
-  params.addCoupledVar("residual_def_level", "Residual deformation level from 0 to 1");
+  params.addRequiredCoupledVar("residual_def_level", "Residual deformation level from 0 to 1");
 
   // Let's check the range of the parameter here
   params.addRangeCheckedParam<std::vector<Real>>(
       "residual_def_components",
       "residual_def_components_size=9",
-      "Vector of values defining the maximum magnitude of the components" 
+      "Vector of values defining the maximum magnitude of the components"
 	  " of the residual deformation tensor that will be applied when residual_def_level = 1."
 	  " Note that these components refers to the beta tensor, meaning the actual eigenstrain"
 	  " deformation gradient will be: I+beta."
 	  " Note the matrix is filled in the following order:"
 	  " 00, 10, 20, 01, 11, 21, 02, 12, 22");
-	  
+
   params.addParam<UserObjectName>("read_initial_residual_def",
                                   "The ElementReadPropertyFile "
                                   "GeneralUserObject to read element value "
@@ -48,15 +48,15 @@ ComputeCrystalPlasticityResidualEigenstrain::ComputeCrystalPlasticityResidualEig
     _ddeformation_gradient_dlevel(declarePropertyDerivative<RankTwoTensor>(
         _deformation_gradient_name, getVar("residual_def_level", 0)->name())),
     _residual_def_components(getParam<std::vector<Real>>("residual_def_components")),
-	
-    // UserObject to read the components of the initial residual deformation from file						
+
+    // UserObject to read the components of the initial residual deformation from file
     _read_initial_residual_def(isParamValid("read_initial_residual_def")
                                ? &getUserObject<ElementPropertyReadFile>("read_initial_residual_def")
                                : nullptr),
-							   
+
     _residual_def(declareProperty<RankTwoTensor>(
         _eigenstrain_name +
-        "_residual_def")) // avoid duplicated material name by including the eigenstrain name 
+        "_residual_def")) // avoid duplicated material name by including the eigenstrain name
 {
 }
 
@@ -72,21 +72,21 @@ ComputeCrystalPlasticityResidualEigenstrain::initQpStatefulProperties()
   // note the matrix is filled in the following order
   // 00, 10, 20, 01, 11, 21, 02, 12, 22
   if (_read_initial_residual_def) {
-	
+
     // read from file element by element
     for (const auto i : make_range(LIBMESH_DIM)) {
       for (const auto j : make_range(LIBMESH_DIM)) {
-		  
-        _residual_def[_qp](i,j) = 
+
+        _residual_def[_qp](i,j) =
 		_read_initial_residual_def->getData(_current_elem, LIBMESH_DIM*j+i);
-		
+
       }
 	}
   } else {
-	  
+
 	// homogeneous value through the geometry
-    _residual_def[_qp] = _residual_def_components; 
-	
+    _residual_def[_qp] = _residual_def_components;
+
   }
 }
 
@@ -97,7 +97,7 @@ ComputeCrystalPlasticityResidualEigenstrain::computeQpDeformationGradient()
   // this is homogeneous in the geometry
   // when _residual_def_level[_qp] = 1.0
   // the full eigenstrain is applied, therefore no incremental formulation is needed
-  _deformation_gradient[_qp] = RankTwoTensor::Identity() 
+  _deformation_gradient[_qp] = RankTwoTensor::Identity()
                              + _residual_def_level[_qp] * _residual_def[_qp];
 
   // compute the derivative of eigenstrain deformation gradient w.r.t _residual_def_level
