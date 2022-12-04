58b7bd68c11648b316375e74af93942800f5e5ce
Nicolo <nicolo.grilli@bristol.ac.uk>
2022-12-04 01:02:34 +0000

    temperature dependent specific heat class

---
+++ include/kernels/TDepCpHeatTimeDerivative.h
@@ -0,0 +1,39 @@
+// Nicolò Grilli
+// University of Bristol
+// 4 Dicembre 2022
+
+#pragma once
+
+// MOOSE includes
+#include "TimeDerivative.h"
+#include "Material.h"
+
+// Forward Declarations
+
+/**
+ * A class for defining the time derivative of the heat equation.
+ *
+ * By default this Kernel computes:
+ *   \f$ \rho * c_p (T) * \frac{\partial T}{\partial t}, \f$
+ * where \f$ \rho \f$ and \f$ c_p \f$ are material properties with the names "density" and
+ * "specific_heat", respectively.
+ * The specific heat depends linearly on temperature
+ */
+class TDepCpHeatTimeDerivative : public TimeDerivative
+{
+public:
+  /// Contructor for Heat Equation time derivative term.
+  static InputParameters validParams();
+
+  TDepCpHeatTimeDerivative(const InputParameters & parameters);
+
+protected:
+  /// Compute the residual of the Heat Equation time derivative.
+  virtual Real computeQpResidual();
+
+  /// Compute the jacobian of the Heat Equation time derivative.
+  virtual Real computeQpJacobian();
+
+  const MaterialProperty<Real> & _specific_heat;
+  const MaterialProperty<Real> & _density;
+};
---
+++ src/kernels/TDepCpHeatTimeDerivative.C
@@ -0,0 +1,52 @@
+// Nicolò Grilli
+// University of Bristol
+// 4 Dicembre 2022
+
+#include "TDepCpHeatTimeDerivative.h"
+
+registerMooseObject("HeatConductionApp", TDepCpHeatTimeDerivative);
+
+InputParameters
+TDepCpHeatTimeDerivative::validParams()
+{
+  InputParameters params = TimeDerivative::validParams();
+  params.addClassDescription(
+      "Time derivative term $\\rho c_p (T) \\frac{\\partial T}{\\partial t}$ of "
+      "the heat equation for specific heat $c_p$ that depends linearly on temperature "
+      "and the density $\\rho$.");
+
+  // Density may be changing with deformation, so we must integrate
+  // over current volume by setting the use_displaced_mesh flag.
+  params.set<bool>("use_displaced_mesh") = true;
+
+  params.addParam<MaterialPropertyName>(
+      "specific_heat", "specific_heat", "Property name of the specific heat material property");
+
+  /**
+   * We would like to rename this input parameter to 'density' but gratuitous use of
+   * 'density' in the GlobalParams block of many many Bison simulations (for the
+   * Density kernel)prevent us from doing this.
+   */
+  params.addParam<MaterialPropertyName>(
+      "density_name", "density", "Property name of the density material property");
+  return params;
+}
+
+TDepCpHeatTimeDerivative::TDepCpHeatTimeDerivative(const InputParameters & parameters)
+  : TimeDerivative(parameters),
+    _specific_heat(getMaterialProperty<Real>("specific_heat")),
+    _density(getMaterialProperty<Real>("density_name"))
+{
+}
+
+Real
+TDepCpHeatTimeDerivative::computeQpResidual()
+{
+  return _specific_heat[_qp] * _density[_qp] * TimeDerivative::computeQpResidual();
+}
+
+Real
+TDepCpHeatTimeDerivative::computeQpJacobian()
+{
+  return _specific_heat[_qp] * _density[_qp] * TimeDerivative::computeQpJacobian();
+}
