9c9b51333f1eb485a419b8c845957ed91cfea97b
Nicolo <nicolo.grilli@bristol.ac.uk>
2024-03-23 23:24:23 +0000

    start class for cyclic dislocation structures model

---
+++ include/materials/CrystalPlasticityCyclicDislocationStructures.h
@@ -0,0 +1,259 @@
+// Marco Pelegatti
+// Università di Udine
+// Nicolò Grilli
+// Università di Bristol
+// 23 Marzo 2024
+
+#pragma once
+
+#include "CrystalPlasticityDislocationUpdateBase.h"
+#include "ElementPropertyReadFile.h"
+
+class CrystalPlasticityCyclicDislocationStructures;
+
+/**
+ * CrystalPlasticityCyclicDislocationStructures uses the multiplicative decomposition of the
+ * deformation gradient and solves the PK2 stress residual equation at the
+ * intermediate configuration to evolve the material state. The internal
+ * variables are updated using an interative predictor-corrector algorithm.
+ * Backward Euler integration rule is used for the rate equations.
+ * Dislocation based model for crystal plasticity for cyclic load.
+ * with backstress and dislocation structures based on:
+ * Theodore Zirkle, Ting Zhu, David L. McDowell
+ * Micromechanical crystal plasticity back stress evolution within FCC dislocation substructure
+ * International Journal of Plasticity 146 (2021) 103082
+ */
+
+class CrystalPlasticityCyclicDislocationStructures : public CrystalPlasticityDislocationUpdateBase
+{
+public:
+  static InputParameters validParams();
+
+  CrystalPlasticityCyclicDislocationStructures(const InputParameters & parameters);
+
+protected:
+  /**
+   * initializes the stateful properties such as
+   * stress, plastic deformation gradient, slip system resistances, etc.
+   */
+  virtual void initQpStatefulProperties() override;
+  
+  /**
+   * A helper method to rotate the a direction and plane normal system set into
+   * the local crystal lattice orientation as defined by the crystal rotation
+   * tensor from the Elasticity tensor class.
+   * Edge and screw slip directions are also assigned
+   */
+  virtual void calculateSchmidTensor(const unsigned int & number_dislocation_systems,
+                             const std::vector<RealVectorValue> & plane_normal_vector,
+                             const std::vector<RealVectorValue> & direction_vector,
+                             std::vector<RankTwoTensor> & schmid_tensor,
+                             const RankTwoTensor & crysrot);
+
+
+  /**
+   * Sets the value of the current and previous substep iteration slip system
+   * resistance to the old value at the start of the PK2 stress convergence
+   * while loop.
+   */
+  virtual void setInitialConstitutiveVariableValues() override;
+
+  /**
+   * Sets the current slip system resistance value to the previous substep value.
+   * In cases where only one substep is taken (or when the first) substep is taken,
+   * this method just sets the current value to the old slip system resistance
+   * value again.
+   */
+  virtual void setSubstepConstitutiveVariableValues() override;
+
+  /**
+   * Stores the current value of the slip system resistance into a separate
+   * material property in case substepping is needed.
+   */
+  virtual void updateSubstepConstitutiveVariableValues() override;
+
+  virtual bool calculateSlipRate() override;
+  
+  virtual void calculateSlipResistance();
+
+  virtual void
+  calculateEquivalentSlipIncrement(RankTwoTensor & /*equivalent_slip_increment*/) override;
+
+  virtual void calculateConstitutiveSlipDerivative(std::vector<Real> & dslip_dtau) override;
+
+  // Cache the slip system value before the update for the diff in the convergence check
+  virtual void cacheStateVariablesBeforeUpdate() override;
+
+  // Evolution of the state variables 
+  virtual void calculateStateVariableEvolutionRateComponent() override;
+  
+  // Armstrong-Frederick update of the backstress
+  virtual void ArmstrongFrederickBackstressUpdate();
+
+  /*
+   * Finalizes the values of the state variables and slip system resistance
+   * for the current timestep after convergence has been reached.
+   */
+  virtual bool updateStateVariables() override;
+
+  /*
+   * Determines if the state variables, e.g. defect densities, have converged
+   * by comparing the change in the values over the iteration period.
+   */
+  virtual bool areConstitutiveStateVariablesConverged() override;
+  
+  // Slip rate constants
+  const Real _ao;
+  const Real _xm;
+  
+  // Optional xm material property for exponent for slip rate
+  const bool _include_xm_matprop;
+  const MaterialProperty<Real> * _xm_matprop;
+  
+  // Creep rate constants
+  const Real _creep_ao;
+  const Real _creep_xm;
+  
+  // Optional function for creep prefactor. If provided, the creep prefactor can be set as a function of time
+  // This is useful for an initial plastic deformation followed by creep load
+  const Function * const _creep_ao_function;
+  
+  // Cap the absolute value of the slip increment in one time step to _slip_incr_tol
+  const bool _cap_slip_increment;
+
+  // Magnitude of the Burgers vector
+  const Real _burgers_vector_mag;
+  
+  // Shear modulus in Taylor hardening law G
+  const Real _shear_modulus;
+  
+  // Prefactor of Taylor hardening law, alpha
+  const Real _alpha_0;
+  
+  // Latent hardening coefficient
+  const Real _r;
+  
+  // Peierls stress
+  const Real _tau_c_0;
+  
+  // Coefficient K in SSD evolution, representing accumulation rate
+  const Real _k_0;
+  
+  // Critical annihilation diameter
+  const Real _y_c;
+  
+  // Backstress parameters
+  const Real _h;
+  const Real _h_D;
+  
+  // Initial values of the dislocation density
+  const Real _init_rho_c;
+  const Real _init_rho_w;
+  
+  const Real _init_rho_ssd;
+  const Real _init_rho_gnd_edge;
+  const Real _init_rho_gnd_screw;
+  
+  // Tolerance on dislocation density update
+  const Real _rho_tol;
+  
+  // Dislocation densities
+  MaterialProperty<std::vector<Real>> & _rho_c;
+  const MaterialProperty<std::vector<Real>> & _rho_c_old;
+  MaterialProperty<std::vector<Real>> & _rho_w;
+  const MaterialProperty<std::vector<Real>> & _rho_w_old;
+  
+  // Walls fraction
+  MaterialProperty<Real> & _f_w;
+  const MaterialProperty<Real> & _f_w_old;
+
+  MaterialProperty<std::vector<Real>> & _rho_ssd;
+  const MaterialProperty<std::vector<Real>> & _rho_ssd_old;
+  MaterialProperty<std::vector<Real>> & _rho_gnd_edge;
+  const MaterialProperty<std::vector<Real>> & _rho_gnd_edge_old;
+  MaterialProperty<std::vector<Real>> & _rho_gnd_screw;
+  const MaterialProperty<std::vector<Real>> & _rho_gnd_screw_old;
+  
+  // Backstress variables
+  MaterialProperty<std::vector<Real>> & _backstress;
+  const MaterialProperty<std::vector<Real>> & _backstress_old;  
+  
+  /// Increment of dislocation densities and backstress
+  std::vector<Real> _rho_c_increment;
+  std::vector<Real> _rho_w_increment;
+  
+  std::vector<Real> _rho_ssd_increment;
+  std::vector<Real> _rho_gnd_edge_increment;
+  std::vector<Real> _rho_gnd_screw_increment;
+  std::vector<Real> _backstress_increment;
+
+  /**
+   * Stores the values of the dislocation densities and backstress 
+   * from the previous substep
+   * In classes which use dislocation densities, analogous dislocation density
+   * substep vectors will be required.
+   */
+  std::vector<Real> _previous_substep_rho_c;
+  std::vector<Real> _previous_substep_rho_w;
+   
+  std::vector<Real> _previous_substep_rho_ssd;
+  std::vector<Real> _previous_substep_rho_gnd_edge;
+  std::vector<Real> _previous_substep_rho_gnd_screw;
+  std::vector<Real> _previous_substep_backstress;
+
+  /**
+   * Caches the value of the current dislocation densities immediately prior
+   * to the update, and they are used to calculate the
+   * the dislocation densities for the current substep (or step if
+   * only one substep is taken) for the convergence check tolerance comparison.
+   */
+  std::vector<Real> _rho_c_before_update;
+  std::vector<Real> _rho_w_before_update;   
+   
+  std::vector<Real> _rho_ssd_before_update;
+  std::vector<Real> _rho_gnd_edge_before_update; 
+  std::vector<Real> _rho_gnd_screw_before_update;
+  std::vector<Real> _backstress_before_update;
+
+  /**
+   * Flag to include the total twin volume fraction in the plastic velocity
+   * gradient calculation, per Kalidindi IJP (2001).
+   */
+  const bool _include_twinning_in_Lp;
+
+  /**
+   * User-defined material property name for the total volume fraction of twins
+   * in a twinning propagation constitutive model, when this class is used in
+   * conjunction with the twinning propagation model.
+   * Note that this value is the OLD material property and thus lags the current
+   * value by a single timestep.
+   */
+  const MaterialProperty<Real> * const _twin_volume_fraction_total;
+  
+  /**
+   * UserObject to read the initial GND density from file
+   */
+  const ElementPropertyReadFile * const _read_initial_gnd_density;
+  
+  // Directional derivative of the slip rate along the edge dislocation motion direction
+  // and along the screw dislocation motion direction
+  const ArrayVariableValue & _dslip_increment_dedge;
+  const ArrayVariableValue & _dslip_increment_dscrew;
+  
+  // Temperature dependent properties
+  const VariableValue & _temperature;
+  const Real _reference_temperature;
+  const Real _dCRSS_dT_A;
+  const Real _dCRSS_dT_B;
+  const Real _dCRSS_dT_C;
+  
+  // Rotated slip direction to calculate the directional derivative
+  // of the plastic strain rate
+  // it indicates the edge dislocation velocity direction for all slip systems
+  MaterialProperty<std::vector<Real>> & _edge_slip_direction;
+  
+  // edge dislocation line direction
+  // corresponding to direction of motion of screw dislocations
+  MaterialProperty<std::vector<Real>> & _screw_slip_direction;
+  
+};
