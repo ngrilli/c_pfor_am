8a17a1640d804e6d52542605f3420ed931c4569d
ngrilli <nicolo.grilli@bristol.ac.uk>
2025-01-08 16:16:35 +0000

    stub for Chaboche model

---
+++ include/materials/Chaboche.h
@@ -0,0 +1,54 @@
+// Nicolò Grilli
+// Università di Bristol
+// 7 Gennaio 2025
+
+#pragma once
+
+#include "ComputeStressBase.h"
+
+/**
+ * A Chaboche model with return mapping that follows the implementation in
+ * 0. S. Hopperstad and S. Remseth
+ * A return mapping algorithm for a class of cyclic plasticity models
+ * International Journal for Numerical Methods in Engineering, Vol. 38, 549-564 (1995)
+ */
+class Chaboche : public ComputeStressBase
+{
+public:
+  static InputParameters validParams();
+
+  Chaboche(const InputParameters & parameters);
+
+protected:
+  virtual void computeQpStress();
+  virtual void initQpStatefulProperties();
+
+  // epsilon^p
+  MaterialProperty<RankTwoTensor> & _plastic_strain;
+  const MaterialProperty<RankTwoTensor> & _plastic_strain_old;
+  
+  // p
+  MaterialProperty<Real> & _eqv_plastic_strain;
+  const MaterialProperty<Real> & _eqv_plastic_strain_old;
+
+  // The stress tensor at previous time step
+  const MaterialProperty<RankTwoTensor> & _stress_old;
+
+  // delta epsilon
+  const MaterialProperty<RankTwoTensor> & _strain_increment;
+  
+  // Rotation increment in small strain formulation
+  const MaterialProperty<RankTwoTensor> & _rotation_increment;
+  
+  // Name of the elasticity tensor material property
+  const std::string _elasticity_tensor_name;
+
+  // Elasticity tensor material property
+  const MaterialProperty<RankFourTensor> & _elasticity_tensor;
+  
+  // Backstress tensors
+  MaterialProperty<RankTwoTensor> & _backstress1;
+  const MaterialProperty<RankTwoTensor> & _backstress1_old;
+  MaterialProperty<RankTwoTensor> & _backstress2;
+  const MaterialProperty<RankTwoTensor> & _backstress2_old;
+};
---
+++ src/materials/Chaboche.C
@@ -0,0 +1,63 @@
+// Nicolò Grilli
+// Università di Bristol
+// 7 Gennaio 2025
+
+#include "Chaboche.h"
+
+registerMooseObject("c_pfor_amApp", Chaboche);
+
+InputParameters
+Chaboche::validParams()
+{
+  InputParameters params = ComputeStressBase::validParams();
+
+  params.addClassDescription("A Chaboche model with return mapping that follows the implementation in "
+                             "0. S. Hopperstad and S. Remseth "
+                             "A return mapping algorithm for a class of cyclic plasticity models "
+                             "International Journal for Numerical Methods in Engineering, Vol. 38, 549-564 (1995) ");
+  params.addRequiredParam<Real>("sigma_0","Constant stress of the isotropic hardening");
+  return params;
+}
+
+Chaboche::Chaboche(const InputParameters & parameters)
+  : ComputeStressBase(parameters),
+    _plastic_strain(declareProperty<RankTwoTensor>(_base_name + "plastic_strain")),
+    _plastic_strain_old(getMaterialPropertyOld<RankTwoTensor>(_base_name + "plastic_strain")),
+    _eqv_plastic_strain(declareProperty<Real>(_base_name + "eqv_plastic_strain")),
+    _eqv_plastic_strain_old(getMaterialPropertyOld<Real>(_base_name + "eqv_plastic_strain")),
+    _stress_old(getMaterialPropertyOld<RankTwoTensor>(_base_name + "stress")),
+    _strain_increment(getMaterialProperty<RankTwoTensor>(_base_name + "strain_increment")),
+    _rotation_increment(getMaterialProperty<RankTwoTensor>(_base_name + "rotation_increment")),
+    _elasticity_tensor_name(_base_name + "elasticity_tensor"),
+    _elasticity_tensor(getMaterialPropertyByName<RankFourTensor>(_elasticity_tensor_name)),
+    _backstress1(declareProperty<RankTwoTensor>(_base_name + "backstress1")),
+    _backstress1_old(getMaterialPropertyOld<RankTwoTensor>(_base_name + "backstress1")),
+    _backstress2(declareProperty<RankTwoTensor>(_base_name + "backstress2")),
+    _backstress2_old(getMaterialPropertyOld<RankTwoTensor>(_base_name + "backstress2"))
+{
+}
+
+void
+Chaboche::initQpStatefulProperties()
+{
+  ComputeStressBase::initQpStatefulProperties();
+  _plastic_strain[_qp].zero();
+  _eqv_plastic_strain[_qp] = 0.0;
+}
+
+void
+Chaboche::computeQpStress()
+{
+
+  // Rotate the stress tensor to the current configuration
+  _stress[_qp] = _rotation_increment[_qp] * _stress[_qp] * _rotation_increment[_qp].transpose();
+
+  // Rotate plastic strain tensor to the current configuration
+  //_plastic_strain[_qp] =
+  //    _rotation_increment[_qp] * _plastic_strain[_qp] * _rotation_increment[_qp].transpose();
+
+  // Calculate the elastic strain_increment
+  //_elastic_strain[_qp] = _mechanical_strain[_qp] - _plastic_strain[_qp];
+
+  _Jacobian_mult[_qp] = _elasticity_tensor[_qp];
+}
