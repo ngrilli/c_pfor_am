b23e8f2dde0bb181cdfb2c265d92792655de59b1
nicolo.grilli <nicolo.grilli@bristol.ac.uk>
2025-08-21 18:43:58 +0100

    add substep functions for backstress

--- include/materials/CrystalPlasticityCopper.h
+++ include/materials/CrystalPlasticityCopper.h
@@ -32,8 +32,49 @@ protected:
    * initial lattice friction strength of the material
    */
   virtual void initQpStatefulProperties() override;
+  
+  /**
+   * Sets the value of the current and previous substep iteration slip system
+   * resistance to the old value at the start of the PK2 stress convergence
+   * while loop.
+   */
+  virtual void setInitialConstitutiveVariableValues() override;
+  
+  /**
+   * Sets the current state variables value to the previous substep value.
+   * In cases where only one substep is taken (or when the first) substep is taken,
+   * this method just sets the current value to the old state variables
+   * value again.
+   */
+  virtual void setSubstepConstitutiveVariableValues() override;
 
   /// Standard deviation of the spatial distribution of the
   /// initial lattice friction strength of the material
   const Real _gss_initial_std;
+  
+  /// Backstress parameters
+  const Real _h;
+  const Real _h_D;
+  
+  /// Backstress variables
+  MaterialProperty<std::vector<Real>> & _backstress;
+  const MaterialProperty<std::vector<Real>> & _backstress_old;
+  
+  /// Increment of state variables
+  std::vector<Real> _backstress_increment;
+
+  /**
+   * Stores the values of the state variables
+   * from the previous substep
+   */
+  std::vector<Real> _previous_substep_backstress;
+
+  /**
+   * Caches the value of the current state variables immediately prior
+   * to the update, and they are used to calculate the
+   * the state variables for the current substep (or step if
+   * only one substep is taken) for the convergence check tolerance comparison
+   */
+  std::vector<Real> _backstress_before_update;
+  
 };
--- src/materials/CrystalPlasticityCopper.C
+++ src/materials/CrystalPlasticityCopper.C
@@ -15,13 +15,23 @@ CrystalPlasticityCopper::validParams()
   params.addClassDescription("Phenomenological model for copper with spatially random gss_initial.");
   params.addParam<Real>("gss_initial_std", 0.0, "Standard deviation of the spatial distribution of the " 
                                                 "initial lattice friction strength of the material.");
+  params.addParam<Real>("h",0.0,"Direct hardening coefficient for backstress");
+  params.addParam<Real>("h_D",0.0,"Dynamic recovery coefficient for backstress");
   return params;
 }
 
 CrystalPlasticityCopper::CrystalPlasticityCopper(
     const InputParameters & parameters)
   : CrystalPlasticityKalidindiUpdate(parameters),
-    _gss_initial_std(getParam<Real>("gss_initial_std"))
+    _gss_initial_std(getParam<Real>("gss_initial_std")),
+    
+    // Backstress parameters
+    _h(getParam<Real>("h")),
+    _h_D(getParam<Real>("h_D")),
+
+    // Backstress variable
+    _backstress(declareProperty<std::vector<Real>>("backstress")),
+    _backstress_old(getMaterialPropertyOld<std::vector<Real>>("backstress"))
 {
 }
 
@@ -33,6 +43,9 @@ CrystalPlasticityCopper::initQpStatefulProperties()
   
   Real rand_num;
   
+  // Initialize the backstress size
+  _backstress[_qp].resize(_number_slip_systems);
+  
   for (const auto i : make_range(_number_slip_systems))
   {
     // Random number between 0 and 1
@@ -40,5 +53,24 @@ CrystalPlasticityCopper::initQpStatefulProperties()
     
     _slip_resistance[_qp][i] += _gss_initial_std * (2.0 * rand_num - 1.0);
     // TO DO: add check that slip resistance remains positive
+    
+    // Initialize backstress
+    _backstress[_qp][i] = 0.0;
   }
+}
+
+void
+CrystalPlasticityCopper::setInitialConstitutiveVariableValues()
+{
+  // Initialize state variables with the value at the previous time step
+  _backstress[_qp] = _backstress_old[_qp];
+  _previous_substep_backstress = _backstress_old[_qp];
+}
+
+void
+CrystalPlasticityCopper::setSubstepConstitutiveVariableValues()
+{
+  // Inialize state variable of the next substep
+  // with the value at the previous substep
+  _backstress[_qp] = _previous_substep_backstress;
 }
