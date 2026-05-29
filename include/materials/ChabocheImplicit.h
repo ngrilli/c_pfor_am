5e7c56b1c34988dc6e5b3ef52b12f07580d1bd23
ngrilli <nicolo.grilli@bristol.ac.uk>
2026-05-29 01:23:34 +0100

    version of Chaboche model with implicit update of backstress

---
+++ include/materials/ChabocheImplicit.h
@@ -0,0 +1,142 @@
+// Nicolò Grilli
+// Università di Bristol
+// 7 Gennaio 2025
+
+#pragma once
+
+#include "ComputeStressBase.h"
+
+#include "RankTwoTensor.h"
+#include "RankFourTensor.h"
+
+/**
+ * A Chaboche model with return mapping
+ */
+class ChabocheImplicit : public ComputeStressBase
+{
+public:
+  static InputParameters validParams();
+
+  ChabocheImplicit(const InputParameters & parameters);
+
+protected:
+  virtual void initQpStatefulProperties();
+  virtual void computeQpStress();
+  
+  /// Compute shear and bulk modulus
+  virtual void computeElasticConstants();
+  
+  /// Decompose stress into deviatoric and volumetric
+  virtual void decomposeStress(const RankTwoTensor & stress_old);
+  
+  virtual void decomposeStrainIncrement(const RankTwoTensor & strain_increment);
+  
+  virtual RankTwoTensor computeTrialStress();
+  /**
+   * Calculate Mises equivalent stress using the secondInvariant function
+   * which is the Mises stress when multiplied by a factor 3
+   */
+  Real getMisesEquivalent(const RankTwoTensor & stress);
+  
+  virtual Real yieldFunction(const RankTwoTensor & effective_deviatoric_stress,
+                             const Real yield_stress);
+                             
+  virtual void returnMap(const Real eqvpstrain_old,
+                         const RankTwoTensor & plastic_strain_old,
+                         Real & eqvpstrain,
+                         RankTwoTensor & plastic_strain);
+                         
+  virtual Real updateIsotropicHardening(const Real eqvpstrain);                 
+                         
+  /// update backstress after return mapping converged
+  virtual RankTwoTensor updateBackstress(const Real delta_gamma,
+                                         const RankTwoTensor n,
+                                         const unsigned int backstress_index);
+
+  virtual Real numericalJacobian(const Real delta_gamma,
+                                 const Real eqvpstrain_old,
+                                 const RankTwoTensor n,
+                                 const Real residual);
+                                
+  virtual void elastoPlasticTangentModuli(const Real eqvpstrain,
+                                          const RankTwoTensor n);
+
+  // epsilon^p
+  MaterialProperty<RankTwoTensor> & _plastic_strain;
+  const MaterialProperty<RankTwoTensor> & _plastic_strain_old;
+  
+  // p
+  MaterialProperty<Real> & _eqv_plastic_strain;
+  const MaterialProperty<Real> & _eqv_plastic_strain_old;
+
+  // The stress tensor at previous time step
+  const MaterialProperty<RankTwoTensor> & _stress_old;
+
+  // delta epsilon_{n+1}
+  const MaterialProperty<RankTwoTensor> & _strain_increment;
+  
+  // Rotation increment in small strain formulation
+  const MaterialProperty<RankTwoTensor> & _rotation_increment;
+  
+  // Name of the elasticity tensor material property
+  const std::string _elasticity_tensor_name;
+
+  // Elasticity tensor material property
+  const MaterialProperty<RankFourTensor> & _elasticity_tensor;
+  
+  // Backstress tensors
+  MaterialProperty<RankTwoTensor> & _backstress1;
+  const MaterialProperty<RankTwoTensor> & _backstress1_old;
+  MaterialProperty<RankTwoTensor> & _backstress2;
+  const MaterialProperty<RankTwoTensor> & _backstress2_old;
+  
+  // Isotropic hardening R
+  MaterialProperty<Real> & _isotropic_hardening;
+  const MaterialProperty<Real> & _isotropic_hardening_old;
+  
+  // Isotropic hardening parameters
+  const MaterialProperty<Real> & _sigma_0;
+  const MaterialProperty<Real> & _Q;
+  const MaterialProperty<Real> & _b;
+  
+  // Young's modulus and Poisson's ratio as a function of temperature 
+  const MaterialProperty<Real> & _E;
+  const MaterialProperty<Real> & _nu;
+  
+  // Backstress parameters
+  const MaterialProperty<Real> & _C1;
+  const MaterialProperty<Real> & _gamma1;
+  const MaterialProperty<Real> & _C2;
+  const MaterialProperty<Real> & _gamma2;
+  
+  // Yield function tolerance
+  const Real _tolerance;
+  
+  // Number of return mapping iterations before unconverged
+  const int _max_iterations;
+
+  /// Type of tangent moduli calculation
+  const enum class TangentModuliType { ELASTIC, ELASTO_PLASTIC } _tan_mod_type;
+  
+  // Shear, bulk modulus and first Lamé parameter
+  Real _G;
+  Real _K;
+  Real _lambda;
+  
+  // Volumetric and deviatoric stress  
+  RankTwoTensor _deviatoric_stress_old;
+  RankTwoTensor _volumetric_stress_old;
+  
+  // Volumetric and deviatoric strain increment
+  RankTwoTensor _deviatoric_strain_increment;
+  RankTwoTensor _volumetric_strain_increment;
+  
+  // Deviatoric trial stress
+  RankTwoTensor _trial_stress;
+  
+  // Return mapping variables changing at each iteration
+  // they represent the quantity at the next time step once the return mapping converged
+  RankTwoTensor _effective_deviatoric_stress;
+  RankTwoTensor _backstress1_iter;
+  RankTwoTensor _backstress2_iter;
+};
---
+++ src/materials/ChabocheImplicit.C
@@ -0,0 +1,346 @@
+// Nicolò Grilli
+// Università di Bristol
+// 28 Maggio 2026
+
+#include "ChabocheImplicit.h"
+
+registerMooseObject("c_pfor_amApp", ChabocheImplicit);
+
+InputParameters
+ChabocheImplicit::validParams()
+{
+  InputParameters params = ComputeStressBase::validParams();
+
+  params.addClassDescription("A Chaboche model with return mapping, implicit version");
+  params.addParam<MaterialPropertyName>("sigma_0_name","sigma_0","Constant stress of the isotropic hardening");
+  params.addParam<MaterialPropertyName>("Q_name","Q","Maximum isotropic hardening");
+  params.addParam<MaterialPropertyName>("b_name","b","Hardening rate");
+  params.addParam<MaterialPropertyName>("E_name","E","Young's modulus");
+  params.addParam<MaterialPropertyName>("nu_name","nu","Poisson's ratio");
+  params.addParam<MaterialPropertyName>("C1_name","C1","C constant for the first backstress");
+  params.addParam<MaterialPropertyName>("gamma1_name","gamma1","gamma constant for the first backstress");
+  params.addParam<MaterialPropertyName>("C2_name","C2","C constant for the second backstress");
+  params.addParam<MaterialPropertyName>("gamma2_name","gamma2","gamma constant for the second backstress");
+  params.addParam<Real>("tolerance",1e-6,"Yield function tolerance");
+  params.addParam<int>("max_iterations",1000,"Number of return mapping iterations before unconverged");
+  params.addParam<MooseEnum>("tangent_moduli_type",
+                             MooseEnum("elastic elasto_plastic", "elastic"),
+                             "Type of tangent moduli for preconditioner: default elastic");
+  return params;
+}
+
+ChabocheImplicit::ChabocheImplicit(const InputParameters & parameters)
+  : ComputeStressBase(parameters),
+    _plastic_strain(declareProperty<RankTwoTensor>(_base_name + "plastic_strain")),
+    _plastic_strain_old(getMaterialPropertyOld<RankTwoTensor>(_base_name + "plastic_strain")),
+    _eqv_plastic_strain(declareProperty<Real>(_base_name + "eqv_plastic_strain")),
+    _eqv_plastic_strain_old(getMaterialPropertyOld<Real>(_base_name + "eqv_plastic_strain")),
+    _stress_old(getMaterialPropertyOld<RankTwoTensor>(_base_name + "stress")),
+    _strain_increment(getMaterialProperty<RankTwoTensor>(_base_name + "strain_increment")),
+    _rotation_increment(getMaterialProperty<RankTwoTensor>(_base_name + "rotation_increment")),
+    _elasticity_tensor_name(_base_name + "elasticity_tensor"),
+    _elasticity_tensor(getMaterialPropertyByName<RankFourTensor>(_elasticity_tensor_name)),
+    _backstress1(declareProperty<RankTwoTensor>(_base_name + "backstress1")),
+    _backstress1_old(getMaterialPropertyOld<RankTwoTensor>(_base_name + "backstress1")),
+    _backstress2(declareProperty<RankTwoTensor>(_base_name + "backstress2")),
+    _backstress2_old(getMaterialPropertyOld<RankTwoTensor>(_base_name + "backstress2")),
+    _isotropic_hardening(declareProperty<Real>(_base_name + "isotropic_hardening")),
+    _isotropic_hardening_old(getMaterialPropertyOld<Real>(_base_name + "isotropic_hardening")),
+    _sigma_0(getMaterialPropertyOld<Real>(getParam<MaterialPropertyName>("sigma_0_name"))),
+    _Q(getMaterialPropertyOld<Real>(getParam<MaterialPropertyName>("Q_name"))),
+    _b(getMaterialPropertyOld<Real>(getParam<MaterialPropertyName>("b_name"))),
+    _E(getMaterialPropertyOld<Real>(getParam<MaterialPropertyName>("E_name"))),
+    _nu(getMaterialPropertyOld<Real>(getParam<MaterialPropertyName>("nu_name"))),
+    _C1(getMaterialPropertyOld<Real>(getParam<MaterialPropertyName>("C1_name"))),
+    _gamma1(getMaterialPropertyOld<Real>(getParam<MaterialPropertyName>("gamma1_name"))),
+    _C2(getMaterialPropertyOld<Real>(getParam<MaterialPropertyName>("C2_name"))),
+    _gamma2(getMaterialPropertyOld<Real>(getParam<MaterialPropertyName>("gamma2_name"))),
+    _tolerance(getParam<Real>("tolerance")),
+    _max_iterations(getParam<int>("max_iterations")),
+    _tan_mod_type(getParam<MooseEnum>("tangent_moduli_type").getEnum<TangentModuliType>())
+{
+}
+
+void
+ChabocheImplicit::initQpStatefulProperties()
+{
+  ComputeStressBase::initQpStatefulProperties();
+  _plastic_strain[_qp].zero();
+  _eqv_plastic_strain[_qp] = 0.0;
+  _backstress1[_qp].zero();
+  _backstress2[_qp].zero();
+  _isotropic_hardening[_qp] = _sigma_0[_qp];
+}
+
+void
+ChabocheImplicit::computeQpStress()
+{
+  // Compute elastic stiffness
+  computeElasticConstants();
+  
+  // Decompose stress
+  decomposeStress(_stress_old[_qp]);
+  
+  // Decompose strain increment
+  decomposeStrainIncrement(_strain_increment[_qp]);
+
+  // Compute trial stress: _trial_stress is deviatoric
+  _trial_stress = computeTrialStress();
+  
+  // Assume first this strain increment does not induce any plasticity
+  _eqv_plastic_strain[_qp] = _eqv_plastic_strain_old[_qp];
+  _plastic_strain[_qp] = _plastic_strain_old[_qp];
+  _backstress1[_qp] = _backstress1_old[_qp];
+  _backstress2[_qp] = _backstress2_old[_qp];
+  _isotropic_hardening[_qp] = _isotropic_hardening_old[_qp];
+  
+  // Compute effective stress
+  _effective_deviatoric_stress = _trial_stress - _backstress1[_qp] - _backstress2[_qp]; 
+  
+  if (yieldFunction(_effective_deviatoric_stress,_isotropic_hardening[_qp]) >= 0.0) {
+    
+    returnMap(_eqv_plastic_strain_old[_qp],
+              _plastic_strain_old[_qp],
+              _eqv_plastic_strain[_qp],
+              _plastic_strain[_qp]);
+    
+  } else { // purely elastic stress update
+
+    _stress[_qp] = _stress_old[_qp] + 2.0 * _G * _volumetric_strain_increment;
+    _stress[_qp].addIa(_lambda * _volumetric_strain_increment.trace());
+    _stress[_qp] += 2.0 * _G * _deviatoric_strain_increment;
+    _Jacobian_mult[_qp] = _elasticity_tensor[_qp];
+  }
+
+  // Rotate the stress tensor to the current configuration
+  _stress[_qp] = _rotation_increment[_qp] * _stress[_qp] * _rotation_increment[_qp].transpose();
+
+  // Rotate plastic strain tensor to the current configuration
+  _plastic_strain[_qp] =
+      _rotation_increment[_qp] * _plastic_strain[_qp] * _rotation_increment[_qp].transpose();
+
+  // Calculate the elastic strain
+  _elastic_strain[_qp] = _mechanical_strain[_qp] - _plastic_strain[_qp];
+}
+
+// Compute shear and bulk modulus
+void
+ChabocheImplicit::computeElasticConstants()
+{
+  _G = _E[_qp] / 2.0 / (1.0 + _nu[_qp]);
+  _K = _E[_qp] / 3.0 / (1.0 - 2.0 * _nu[_qp]);
+  _lambda = _K - (2.0 / 3.0) * _G;
+}
+
+void
+ChabocheImplicit::decomposeStress(const RankTwoTensor & stress_old)
+{
+  _deviatoric_stress_old = stress_old.deviatoric();
+  _volumetric_stress_old.zero();
+  _volumetric_stress_old.addIa(stress_old.trace() / 3.0);
+}
+
+void
+ChabocheImplicit::decomposeStrainIncrement(const RankTwoTensor & strain_increment)
+{
+  _deviatoric_strain_increment = strain_increment.deviatoric();
+  _volumetric_strain_increment.zero();
+  _volumetric_strain_increment.addIa(strain_increment.trace() / 3.0);
+}
+
+// Trial stress assuming the entire strain increment is elastic
+// Trial stress includes only the deviatoric component
+RankTwoTensor
+ChabocheImplicit::computeTrialStress()
+{
+  return _deviatoric_stress_old + 2.0 * _G * _deviatoric_strain_increment;
+}
+
+// Calculate Mises equivalent stress
+// this calculation intrinsically removes volumetric components
+Real
+ChabocheImplicit::getMisesEquivalent(const RankTwoTensor & stress)
+{
+  return std::sqrt(3 * stress.secondInvariant());
+}
+
+// Yield function, plasticity if positive
+Real
+ChabocheImplicit::yieldFunction(const RankTwoTensor & effective_deviatoric_stress,
+                                const Real yield_stress)
+{
+  return getMisesEquivalent(effective_deviatoric_stress) - yield_stress;
+}
+
+void
+ChabocheImplicit::returnMap(const Real eqvpstrain_old,
+                            const RankTwoTensor & plastic_strain_old,
+                            Real & eqvpstrain,
+                            RankTwoTensor & plastic_strain)
+{
+  // scalar plastic multiplier that is solved for
+  Real delta_gamma = 0.0;
+  
+  // Residual and Jacobian as a function of delta_gamma
+  // Jacobian is numerical
+  Real residual;
+  Real jacobian;
+  
+  // Direction of plastic flow, in tensor form
+  RankTwoTensor n;
+  
+  // Scalar plastic strain increment
+  Real delta_eps_p;
+  
+  // Mises equivalent of the effective deviatoric stress
+  Real eqv_effective_deviatoric_stress;
+  
+  // Isotropic hardening parameters
+  Real Q = _Q[_qp];
+  Real b = _b[_qp];
+
+  // Initialize temporary backstress variables for the return mapping iterations
+  _backstress1_iter = _backstress1[_qp];
+  _backstress2_iter = _backstress2[_qp];
+
+  // Initialize direction of plastic flow
+  eqv_effective_deviatoric_stress = getMisesEquivalent(_effective_deviatoric_stress);
+  n = _effective_deviatoric_stress / eqv_effective_deviatoric_stress;
+
+  // Iteration counter
+  unsigned int i = 0;
+  
+  do {
+    // Update plastic strain increment
+    delta_eps_p = std::sqrt(2.0 / 3.0) * std::abs(delta_gamma);    
+    
+    // Update isotropic hardening
+    eqvpstrain = eqvpstrain_old + delta_eps_p;
+    _isotropic_hardening[_qp] = updateIsotropicHardening(eqvpstrain);
+
+    // Implicit backstress update
+    _backstress1_iter = updateBackstress(delta_gamma, n, 0);
+    _backstress2_iter = updateBackstress(delta_gamma, n, 1);
+
+    // Update effective deviatoric stress
+    _effective_deviatoric_stress = _trial_stress - _backstress1_iter - _backstress2_iter - 2.0 * _G * delta_gamma * n;
+    eqv_effective_deviatoric_stress = getMisesEquivalent(_effective_deviatoric_stress);
+    n = _effective_deviatoric_stress / eqv_effective_deviatoric_stress;
+    
+    // Calculate residual
+    residual = eqv_effective_deviatoric_stress - _isotropic_hardening[_qp];
+
+    // Calculate numerical Jacobian
+    jacobian = numericalJacobian(delta_gamma, eqvpstrain_old, n, residual);
+
+    // Update plastic multiplier
+    delta_gamma -= residual / jacobian;
+    
+    if (i > _max_iterations) // unconverged
+      mooseError("Constitutive failure");
+    
+    // update iteration counter
+    i++;
+  }
+  while (residual > _tolerance);
+  
+  // Update plastic strain, isotropic hardening and back stress
+  delta_eps_p = std::sqrt(2.0 / 3.0) * std::abs(delta_gamma);
+  eqvpstrain = eqvpstrain_old + delta_eps_p;
+  _isotropic_hardening[_qp] = updateIsotropicHardening(eqvpstrain);
+  plastic_strain = plastic_strain_old + delta_gamma * n;
+  _backstress1[_qp] = updateBackstress(delta_gamma, n, 0);
+  _backstress2[_qp] = updateBackstress(delta_gamma, n, 1);
+
+  // Update total stress: recombine deviatoric and hydrostatic parts
+  _stress[_qp] = _stress_old[_qp] + 2.0 * _G * _volumetric_strain_increment;
+  _stress[_qp].addIa(_lambda * _volumetric_strain_increment.trace());
+  _stress[_qp] += 2.0 * _G * _deviatoric_strain_increment - 2.0 * _G * delta_gamma * n;
+
+  switch (_tan_mod_type)
+  {
+    case TangentModuliType::ELASTO_PLASTIC:
+      elastoPlasticTangentModuli(eqvpstrain, n);
+      break;
+    default:
+      _Jacobian_mult[_qp] = _elasticity_tensor[_qp];
+  }
+}
+
+Real
+ChabocheImplicit::updateIsotropicHardening(const Real eqvpstrain)
+{
+  // Isotropic hardening parameters
+  Real Q = _Q[_qp];
+  Real b = _b[_qp];
+  Real sigma_0 = _sigma_0[_qp];
+	
+  return sigma_0 + Q * (1.0 - std::exp(-b * eqvpstrain));
+}
+
+RankTwoTensor
+ChabocheImplicit::updateBackstress(const Real delta_gamma,
+                                   const RankTwoTensor n,
+                                   const unsigned int backstress_index)
+{
+  if (backstress_index == 0) {
+    return (_backstress1_old[_qp] + (2.0 / 3.0) * _C1[_qp] * delta_gamma * n) /
+           (1.0 + _gamma1[_qp] * delta_gamma);
+  } else if (backstress_index == 1) {
+    return (_backstress2_old[_qp] + (2.0 / 3.0) * _C2[_qp] * delta_gamma * n) /
+           (1.0 + _gamma2[_qp] * delta_gamma);
+  } else {
+    mooseError("Invalid backstress index");
+  }
+}
+
+Real
+ChabocheImplicit::numericalJacobian(const Real delta_gamma,
+                                    const Real eqvpstrain_old,
+                                    const RankTwoTensor n,
+                                    const Real residual)
+{
+  // Perturb delta_gamma
+  Real h = 1e-8;
+  Real delta_gamma_perturbed = delta_gamma + h;
+
+  Real delta_eps_p_perturbed = std::sqrt(2.0 / 3.0) * std::abs(delta_gamma_perturbed);
+  Real eqvpstrain_perturbed = eqvpstrain_old + delta_eps_p_perturbed;
+
+  // Update isotropic hardening with perturbed delta_gamma
+  Real isotropic_hardening_perturbed = updateIsotropicHardening(eqvpstrain_perturbed);
+
+  // Update backstress with perturbed delta_gamma
+  RankTwoTensor backstress1_perturbed = updateBackstress(delta_gamma_perturbed, n, 0);
+  RankTwoTensor backstress2_perturbed = updateBackstress(delta_gamma_perturbed, n, 1);
+
+  // Update effective deviatoric stress with perturbed delta_gamma
+  RankTwoTensor effective_deviatoric_stress_perturbed = _trial_stress - backstress1_perturbed - backstress2_perturbed - 2.0 * _G * delta_gamma_perturbed * n;
+  Real eqv_effective_deviatoric_stress_perturbed = getMisesEquivalent(effective_deviatoric_stress_perturbed);
+
+  // Calculate residual with perturbed delta_gamma
+  Real residual_perturbed = eqv_effective_deviatoric_stress_perturbed - isotropic_hardening_perturbed;
+
+  // Calculate numerical Jacobian
+  return (residual_perturbed - residual) / h;
+}
+
+void
+ChabocheImplicit::elastoPlasticTangentModuli(const Real eqvpstrain,
+                                     const RankTwoTensor n)
+{
+  RankFourTensor tan_mod;
+  RankFourTensor I4(RankFourTensor::initIdentityFour); // I tensor product I
+  //RankFourTensor I4dev(RankFourTensor::initIdentityDeviatoric); // deviatoric 4th order identity
+  RankFourTensor I4dev(RankFourTensor::initIdentitySymmetricFour);
+  RankFourTensor n_outer_n; // tensor product of n with itself
+
+  // Hardening rate
+  Real H_iso = _Q[_qp] * _b[_qp] * std::exp(-_b[_qp] * eqvpstrain);
+  Real H_kin = (2.0 / 3.0) * (_C1[_qp] + _C2[_qp]);
+  Real H = H_iso + H_kin;
+
+  n_outer_n = n.outerProduct(n);
+  
+  _Jacobian_mult[_qp] = _K * I4 + 2.0 * _G * (1.0 - (2.0 * _G) / (2.0 * _G + H)) * (I4dev - (3.0/2.0) * n_outer_n);
+}
